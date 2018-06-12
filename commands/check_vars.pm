d36d46a03eceb59157b0affe90cb0ae197b976f0
acadey <acadey@gmail.com>
2018-06-11 21:02:01 -0700

    check vars typo

--- commands/check_vars.pm
+++ commands/check_vars.pm
@@ -8,7 +8,7 @@ use Exporter "import";
 sub check_vars {
 
 @ARGV = @_;
-if (!defined $ARGV[0]) {print "DEBUG: No variables were passed to this command!\n";
+if (!defined $ARGV[0]) {print "DEBUG: No variables were passed to this command!\n"};
 for ($i = 0; $i < @ARGV; $i++) {
 	print "DEBUG: Var ARGV $i passed is $ARGV[$i]\n";
 }
