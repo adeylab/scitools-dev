439c62c8d24a4a6300be26eba2b261d470075823
acadey <adey@ohsu.edu>
2018-06-26 11:06:08 -0700

    added matrix-pull for convenience

---
+++ sci_commands/matrix_pull.pm
@@ -0,0 +1,60 @@
+package sci_commands::matrix_pull;
+
+use sci_utils::general;
+use Getopt::Std; %opt = ();
+use Exporter "import";
+@EXPORT = ("matrix_pull");
+
+sub matrix_pull {
+
+@ARGV = @_;
+
+getopts("F", \%opt);
+
+$die2 = "
+scitools matrix-pull [options] [matrix] [column,row] [column,row] etc... OR [file]
+   or    pull-matrix
+
+Pull a value or list of values from a matrix. Outputs to STDOUT, tab delimited:
+column (tab) row (tab) value
+
+If a file is specified, it should be tab-delimited: column (tab) row
+
+Options:
+   -F   File is the input (will try to auto detect)
+
+";
+
+if (!defined $ARGV[1]) {die $die2};
+
+if (defined $opt{'F'} || $ARGV[1] !~ /,/) {
+	open IN, "$ARGV[1]";
+	while ($l = <IN>) {
+		chomp $l;
+		($col,$row) = split(/\t/, $l);
+		$ROW_COL{$row}{$col} = 1;
+	} close IN;
+} else {
+	for ($i = 1; $i < @ARGV; $i++) {
+		($col,$row) = split(/,/, $ARGV[$i]);
+		$ROW_COL{$row}{$col} = 1;
+	}
+}
+
+open IN, "$ARGV[0]";
+$h = <IN>; chomp $h; @H = split(/\t/, $h);
+while ($l = <IN>) {
+	chomp $l;
+	@P = split(/\t/, $l);
+	$row = shift(@P);
+	if (defined $ROW_COL{$row}) {
+		for ($i = 0; $i < @H; $i++) {
+			if (defined $ROW_COL{$row}{$H[$i]}) {
+				print "$H[$i]\t$row\t$P[$i]\n";
+			}
+		}
+	}
+} close IN;
+
+}
+1;
