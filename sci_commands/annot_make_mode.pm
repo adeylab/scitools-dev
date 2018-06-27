60d3ff79c7ee315e6837cfef543e44a350cd1bd0
acadey <adey@ohsu.edu>
2018-06-27 11:18:41 -0700

    continuing annot-make

--- sci_commands/annot_make_mode.pm
+++ sci_commands/annot_make_mode.pm
@@ -40,6 +40,9 @@ $die2 = "
 scitools annot-make [options] [plate descriptor .csv file] [output prefix]
    or    make-annot
 
+Note: If a multimodal format is specified, an index file will be created for
+each modality.
+
 Options:
    -I   [STR]   Index files/directory - comma separated (req)
          (def = DIR=$VAR{'index_directory'})
@@ -119,17 +122,30 @@ read_indexdir($opt{'I'});
 check_indexes();
 
 # Read in plate descriptor file
-$annotation_sets = load_plate_descriptions($ARGV[0]);
+load_plate_descriptions($ARGV[0]);
 
-
-
-
-
-
-
+# Go through modalities then annots
+foreach $modality (@MODALITIES) {
+	if ($mode->check_multimodal() =~ /true/) {
+		open OUT, ">$ARGV[1].$modality.annot";
+	} else {
+		open OUT, ">$ARGV[1].annot";
+	}
+	foreach $annot (keys %ANNOT_SETS) {
+		@POS_INDEXES = ();
+		for ($index_pos = 0; $index_pos < @{$MODALITY_INDEXES{$modality}}; $index_pos++) {
+			$index_type = $MODALITY_INDEXES{$modality}[$index_pos];
+			$class = $INDEX_TYPE_class{$index_type};
+			if (defined $ANNOT_SETS{$annot}{$class}) {
+				foreach $combo (keys %{$ANNOT_SETS{$annot}{$class}}) {
+					
+				}
+			}
+		}
+	}
+}
 
 #### SUBROUTINES ####
-
 
 sub check_indexes {
 	$absent_index = 0;
@@ -147,9 +163,19 @@ sub check_indexes {
 			if (!defined $INDEX_TYPE_format{$check_index}) {
 				die "ERROR: The index type ($check_index) does not have a format specified! This is specified in the index files as a header.\n";
 			}
+			
+			# build index matrix
+			foreach $index_seq (keys %{$INDEX_TYPE_SEQ_id{$check_index}}) {
+				$index_id = $INDEX_TYPE_SEQ_id{$check_index}{$index_seq};
+				###########################################################
+			}
 		}
 	}
 	if ($absent_index>0) {die "ERROR: Index types specified in mode: $mode_name could not be found in the specified index file(s)/directory(s)!\n"};
+}
+
+sub plate2seq {
+	
 }
 
 sub load_plate_descriptions { # eg: #NEX,MySampleID1,AA,Partial
@@ -161,7 +187,10 @@ sub load_plate_descriptions { # eg: #NEX,MySampleID1,AA,Partial
 		if ($l =~ /^#/) {
 		
 			($class,$annot,$combo,$subset) = split(/,/, $l);
-			$class =~ s/^#//; $annot =~ s/ /_/g;
+			$class =~ s/^#//;
+			if ($class =~ /NEX/i) {$class = "sci_tn5"}; # for reverse compatability
+			if ($class =~ /PCR/i) {$class = "sci_pcr"}; # for reverse compatability
+			$annot =~ s/ /_/g;
 			@CLASS_PARTS = split(/_/, $class);
 			$class = $CLASS_PARTS[0]."_".$CLASS_PARTS[1];
 			
@@ -169,35 +198,20 @@ sub load_plate_descriptions { # eg: #NEX,MySampleID1,AA,Partial
 				die "ERROR: There is no index format specified for index class $class.\n\tThe plate descriptor files must match the index files for the class of the indexes.\n\t(for at least the first two '_' separated fields)\n";
 			}
 			
-			# determine if it is a plate based or row/col based index set
-			if ($INDEX_CLASS_format{$class} =~ /96|all|plate/) {
-				if ($subset =~ /all/i) {
-					for ($wellID = 1; $wellID <= 96; $wellID++) {
-						$ANNOT_SETS{$annot}{$class}{#####################################################
-					}
-				} else {
-					##################################################
-				}
-			} else {
-				($i5_set,$i7_set) = split(//, $combo);
-				
-				
-			}
 			
 			if ($subset =~ /all/i) {
-				for ($rowNum = 1; $rowNum <= 8; $rowNum++) {
-					$rowLetter = $LETTERS[$rowNum];
-					for ($colNum = 1; $colNum <= 12; $colNum++) {
-						
-					}
+				for ($wellID = 1; $wellID <= 96; $wellID++) {
+					$ANNOT_SETS{$annot}{$class}{$combo}{$wellID} = 1;
 				}
 			} else {
 				for ($rowNum = 1; $rowNum <= 8; $rowNum++) {
-					$row = <CSV>; chomp $row; $rowLetter = $LETTERS[$rowNum];
+					$row = <CSV>; chomp $row;
 					@ROW_COLS = split(/,/, $row); unshift @ROW_COLS, "0";
 					for ($colNum = 1; $colNum <= 12; $colNum++) {
-						if ($ROW_COLS[$colNum]>0) {
-							
+						if ($ROW_COLS[$colNum]!=0) {
+							$well_name = $LETTERS[$rowNum].$colNum;
+							$wellID = $PLATE_FORMAT{$well_name};
+							$ANNOT_SETS{$annot}{$class}{$combo}{$wellID} = 1;
 						}
 					}
 				}
@@ -205,80 +219,6 @@ sub load_plate_descriptions { # eg: #NEX,MySampleID1,AA,Partial
 			
 		}
 	} close CSV;
-	
-	return \%ANNOT_SETS;
-}
-
-
-#### OLD
-
-
-if (defined $opt{'P'}) {
-	%NEX_ID_i5_i7_pair = ();
-	%PCR_ID_i5_i7_pair = ();
-	open IN, "$opt{'P'}";
-	while ($l = <IN>) {
-		chomp $l;
-		if ($l =~ /^#/) {
-			($class,$annot,$combo,$subset) = split(/,/, $l);;
-			$class =~ s/^#//; $annot =~ s/ /_/g;
-			if (!defined $ANNOT_flag{$annot}) {
-				$ANNOT_flag{$annot} = $class;
-			} else {
-				$ANNOT_flag{$annot} .= ",$class";
-			}
-			($i5_set,$i7_set) = split(//, $combo);
-			if ($subset =~ /all/i) {
-				for ($rowNum = 1; $rowNum <= 8; $rowNum++) {
-					$rowLetter = $LETTERS[$rowNum];
-					for ($colNum = 1; $colNum <= 12; $colNum++) {
-						if ($class =~ /(Tn5|Nex)/i) {
-							$pair = "$TN5SET_i5WELLS_seq{$i5_set}{$rowLetter},$TN5SET_i7WELLS_seq{$i7_set}{$colNum}";
-							$NEX_ID_i5_i7_pair{$annot}{$pair} = 1;
-						} else {
-							$pair = "$PCRSET_i5WELLS_seq{$i5_set}{$rowLetter},$PCRSET_i7WELLS_seq{$i7_set}{$colNum}";
-							$PCR_ID_i5_i7_pair{$annot}{$pair} = 1;
-						}
-					}
-				}
-			} else {
-				for ($rowNum = 1; $rowNum <= 8; $rowNum++) {
-					$row = <IN>; chomp $row; $rowLetter = $LETTERS[$rowNum];
-					@ROW_COLS = split(/,/, $row); unshift @ROW_COLS, "0";
-					for ($colNum = 1; $colNum <= 12; $colNum++) {
-						if ($ROW_COLS[$colNum]>0) {
-							if ($class =~ /(Tn5|Nex)/i) {
-								$pair = "$TN5SET_i5WELLS_seq{$i5_set}{$rowLetter},$TN5SET_i7WELLS_seq{$i7_set}{$colNum}";
-								$NEX_ID_i5_i7_pair{$annot}{$pair} = 1;
-							} else {
-								$pair = "$PCRSET_i5WELLS_seq{$i5_set}{$rowLetter},$PCRSET_i7WELLS_seq{$i7_set}{$colNum}";
-								$PCR_ID_i5_i7_pair{$annot}{$pair} = 1;
-							}
-						}
-					}
-				}
-			}
-		}
-	} close IN;
-	
-	foreach $annot (keys %ANNOT_flag) {
-		if ($ANNOT_flag{$annot} =~ /(Tn5|Nex)/i && $ANNOT_flag{$annot} =~ /pcr/i) {
-			print STDERR "Printing $annot index combinations.\n";
-			foreach $NEX_pair (keys %{$NEX_ID_i5_i7_pair{$annot}}) {
-				($ix3,$ix1) = split(/,/, $NEX_pair);
-				foreach $PCR_pair (keys %{$PCR_ID_i5_i7_pair{$annot}}) {
-					($ix4,$ix2) = split(/,/, $PCR_pair);
-					if (defined $opt{'O'}) {
-						print OUT "$ix1$ix2$ix3$ix4\t$annot\n";
-					} else {
-						print "$ix1$ix2$ix3$ix4\t$annot\n";
-					}
-				}
-			}
-		} else {
-			print STDERR "\nWARNING: Transposase (Nex/Tn5) AND PCR specifications must both be included for each annotation!\nBoth were not found for $annot! - SKIPPING!\n";
-		}
-	}
 	
 }
 
