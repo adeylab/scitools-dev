4f9dd9baadc95abec7e03f982ae479c97fe2f7f1
Ryan Mulqueen <mulqueenr@gmail.com>
2018-07-13 17:43:47 -0700

    introduce monocle3
    cds file reformatting and monocle3 call, needs to be debugged

---
+++ sci_commands/cds_monocle.pm
@@ -0,0 +1,123 @@
+package sci_commands::cds_monocle;
+
+use sci_utils::general;
+use Getopt::Std; %opt = ();
+use Exporter "import";
+@EXPORT = ("cds_monocle");
+
+sub cds_monocle {
+
+@ARGV = @_;
+# Defaults
+
+getopts("O:R:X:P:D:", \%opt);
+
+$die2 = "
+scitools cds_monocle [options] [directory containing cds files]
+   or    monocle_cds
+
+Runs the current version of Monocle3 within a directory containing files for CDS format input. 
+WIP: Current version performs re-clustering using the monocle pipeline, will adjust to read in scitools generated dims file in the future.
+
+
+Options:
+   -O   [STR]   Output Directory (default is [current working directory]/cds_files)
+   -R   [STR]   Rscript call (def = $Rscript)
+   -D 	[2|3] 	Dimensions to be used for final plotting (2D or 3D plotting)
+   				Default: 2 Dimensions
+   -P 	[INT]	Number of components to be used for dimensionality reduction by PCA to denoise.
+   				Default: 50 components
+   -X           Retain intermediate files (Default = delete)
+                  
+";
+
+
+if (!defined $ARGV[0]) {die $die2};
+if (!defined $opt{'O'}) {$opt{'O'} = $ARGV[0]};
+if (!defined $opt{'D'}) {$opt{'D'} = 2};
+if (!defined $opt{'P'}) {$opt{'P'} = 50};
+
+
+open R, ">$opt{'O'}.monocle.r";
+
+print R "
+library(monocle)
+# reading in matrix, annotation, and dimension data
+# cds_cell_data <- read.delim("./cds_cell_data.txt")
+# cds_dims_data <- read.delim("./cds_dims_data.txt")
+# cds_site_data <- read.delim("./cds_site_data.txt")
+# cds_counts_matrix <- read.delim("./cds_counts_matrix.txt")
+
+cds_cell_data <- read.delim(\"$ARGV[0]/cds_cell_data.txt\")
+#cds_dims_data <- read.delim(\"$ARGV[0]/cds_dims_data.txt\")
+cds_site_data <- read.delim(\"$ARGV[0]/cds_site_data.txt\")
+cds_counts_matrix <- read.table(\"$ARGV[0]/cds_counts_matrix.txt\")
+
+# To be added with dims data update
+#rownames(cds_cell_data)==rownames(cds_dims_data)
+#cds_cell_data<-cbind(cds_cell_data,cds_dims_data)
+
+# generating cell data set
+# feature_data <- new("AnnotatedDataFrame", data = cds_site_data)
+# sample_data <- new("AnnotatedDataFrame", data = cds_cell_data)
+
+feature_data <- new(\"AnnotatedDataFrame\", data = cds_site_data)
+sample_data <- new(\"AnnotatedDataFrame\", data = cds_cell_data)
+
+# To be added with dims data update
+#dimensions_data <- new(\"AnnotatedDataFrame\", data = cds_dims_data)
+
+cds <- newCellDataSet(as.matrix(cds_counts_matrix), phenoData = sample_data, featureData = feature_data)
+set.seed(2017)
+pData(cds)$cells <- NULL
+cds <- detectGenes(cds)
+cds <- estimateSizeFactors(cds)
+cds <- estimateDispersions(cds)
+
+#cds<-preprocessCDS(cds,num_dim=100,use_tf_idf=TRUE,verbose=T)
+
+cds<-preprocessCDS(cds,num_dim=$opt{'P'},use_tf_idf=TRUE,verbose=T)
+#Write out PCA components data frame
+write.table(as.data.frame(cds\@normalized_data_projection), file=\"$opt{'O'}/$opt{'P'}_pca.dims\",quote=F,sep=\"\\t\",row.names=T,col.names=T)
+
+#cds<-reduceDimension(cds,max_components=3,reduction_method="UMAP",metric="cosine",verbose=T)
+cds <- reduceDimension(cds, max_components = $opt{'D'},reduction_method = \'UMAP\',metric=\"cosine\",verbose = T)
+write.table(as.data.frame(t(reducedDimS(cds))), file=\"$opt{'O'}/trajectory_$opt{'D'}D.dims\",quote=F,sep=\"\\t\",row.names=T,col.names=F)
+cds <- partitionCells(cds)
+";
+if ($opt{'D'}==3) {
+print R "
+#3D Plotting
+cds <- learnGraph(cds, max_components = 3, RGE_method = 'SimplePPT', partition_component = T,verbose = T)
+plot_3d_cell_trajectory(cds,color_by=\"annot\",webGL_filename=\"$opt{'O'}/trajectory_3D.html\",image_filename=\"$opt{'O'}/trajectory_3D.gif\",show_backbone=TRUE,useNULL_GLdev=TRUE,backbone_segment_color=\"#000000\")
+";
+} else {
+print R "
+cds <- learnGraph(cds, max_components = 2, RGE_method = 'SimplePPT', partition_component = T,verbose = T)
+p<-plot_cell_trajectory(cds, color_by = \"annot\")
+ggsave(plot=p,filename=\"$opt{'O'}.monocle3.timepoint_plot.png\",width=5,height=4,dpi=900)
+ggsave(plot=p,filename=\"$opt{'O'}.monocle3.timepoint_plot.pdf\",width=5,height=4);
+p<-plot_cell_trajectory(cds, color_by = \"State\")
+ggsave(plot=p,filename=\"$opt{'O'}.monocle3.state_plot.png\",width=5,height=4,dpi=900)
+ggsave(plot=p,filename=\"$opt{'O'}.monocle3.state_plot.pdf\",width=5,height=4);
+#add stuff here if you want to reroot agg_cds <- orderCells(agg_cds, root_state = \"D\")
+p<-plot_cell_trajectory(cds, color_by = \"Pseudotime\")
+ggsave(plot=p,filename=\"$opt{'O'}.monocle3.lambda_plot.png\",width=5,height=4,dpi=900)
+ggsave(plot=p,filename=\"$opt{'O'}.monocle3.lambda_plot.pdf\",width=5,height=4);
+pData(input_cds)\$Pseudotime <- pData(cds)[colnames(),]$Pseudotime
+pData(input_cds)\$State <- pData(cds)[colnames(cds),]$State
+# writing out pseudotime etc so you can recreate everything
+write.table(as.matrix(pData(agg_cds)),file=\"$opt{'O'}/monocle3_cells.txt\",col.names=TRUE,row.names=FALSE,sep=\"\\t\",quote=FALSE)
+write.table(as.matrix(Biobase::exprs(agg_cds)),file=\"$opt{'O'}/monocle3_aggragated_cells_count.txt\",col.names=TRUE,row.names=TRUE,sep=\"\\t\",quote=FALSE)
+write.table(as.matrix(fData(agg_cds)),file=\"$opt{'O'}/monocle3_features.txt\",col.names=TRUE,row.names=FALSE,sep=\"\\t\",quote=FALSE)
+";
+close R;
+system("$Rscript $opt{'O'}.monocle.r");
+if (!defined $opt{'X'}) {
+    system("rm -f $opt{'O'}.monocle.r");
+}
+}
+1;
+
+}
+1;
---
+++ sci_commands/matrix_makeCDS.pm
@@ -0,0 +1,133 @@
+package sci_commands::matrix_makecds;
+
+use sci_utils::general;
+use Getopt::Std; %opt = ();
+use Exporter "import";
+@EXPORT = ("matrix_makecds");
+
+sub matrix_makecds {
+
+@ARGV = @_;
+# Defaults
+
+getopts("O:R:", \%opt);
+
+$die2 = "
+scitools matrix_makecds [options] [input matrix] [annotation file] [dims file]
+   or    ddrtree-matrix
+ Generates CDS format files necessary for Monocle3 and Cicero calls. Places formatted files in a subdirectory.
+ Generates 4 txt files to be used in scitools atac-monocle3 and scitools atac-cicero calls.
+ 1. cds_site_data.txt 		:	Feature set file (called peaks used in counts matrix)
+ 2. cds_cell_data.txt		:	Restructured Annotation file, with timepoints included.
+ 3.	cds_dims_data.txt		:	Restructured dims file
+ 4.	cds_counts_matrix.txt	:	Restructured counts matrix file.
+
+[input matrix]		=	filtered counts matrix
+[annotation file] 	=	<.annot> formatted file, generated through scitools make-annot command
+[dims file]			=	<.dims> formated file, generated through scitools matrix-[umap|swne|pca|irlba|tsne] command
+
+Options:
+   -O   [STR]   Output Directory (default is [current working directory]/cds_files)
+   -R   [STR]   Rscript call (def = $Rscript)
+                  
+";
+
+
+if (!defined $ARGV[0]) {die $die2};
+if (!defined $ARGV[1]) {die $die2};
+if (!defined $ARGV[2]) {die $die2};
+
+if (!defined $opt{'O'}) {$opt{'O'} = $ARGV[0]; $opt{'O'} =~ s/\.matrix$//};
+
+open SITE_DATA, ">./cds_site_data.txt";
+open CELL_DATA, ">./cds_cell_data.txt";
+open DIMS_DATA, ">./cds_dims_data.txt";
+open COUNTS, ">./cds_counts_matrix.txt";
+
+read_annot($ARGV[1]);
+read_dims($ARGV[2]);
+
+#make sure annot matches matrix
+open IN, "$ARGV[0]";
+$h = <IN>; chomp $h; @H = split(/\t/, $h);
+$h_out = "";
+for ($i = 0; $i < @H; $i++) {
+	if ((defined $CELLID_annot{$H[$i]}) && defined ($CELLID_DIMS{$H[$i]})) {
+		$h_out .= "$H[$i]\t";
+	}
+}
+#finish printing out header by removing last \t
+$h_out =~ s/\t$//;
+print COUNTS "$h_out\n";
+#print out header
+
+print SITE_DATA "site_name\tchr\tbp1\tbp2\tnum_cells_expressed\tsite_length\n";
+while ($l = <IN>) {
+	chomp $l;
+	@P = split(/\t/, $l);
+	$site = shift(@P);
+	($chr,$bp1,$bp2) = split(/[_]/, $site);
+	$siteName = "$chr\_$bp1\_$bp2";
+	$siteOut = "$siteName";
+	$siteOut2 = "$siteName";
+	$chrID = $chr; $chrID =~ s/chr//;
+	my $SITENAME_maxSignal=0;
+	for ($i = 0; $i < @P; $i++) {
+		if ((defined $CELLID_annot{$H[$i]}) && defined ($CELLID_DIMS{$H[$i]})) {
+			$SITENAME_totalSignal{$siteName}+=$P[$i];
+			
+			if ($P[$i] > $SITENAME_maxSignal{$siteName}){
+				$SITENAME_maxSignal{$siteName}=$P[$i];
+				#print $siteName."\t$P[$i]\t".$SITENAME_maxSignal{$siteName}."\n";
+			}
+			
+			if ($P[$i]>0) {
+				$SITENAME_expressed{$siteName}++;
+			}
+			
+			
+			
+			if ($P[$i]>0) {
+				$CELLID_expressed{$H[$i]}++;
+			}
+			$siteOut .= "\t$P[$i]";
+		}
+	}
+	print COUNTS "$siteOut\n";
+	
+	
+	for ($i = 0; $i < @P; $i++) 
+		{
+			if ((defined $CELLID_annot{$H[$i]}) && defined ($CELLID_DIMS{$H[$i]})) 
+			{
+		
+			if ($P[$i]>0) {
+				$tempval = $P[$i]/$SITENAME_maxSignal{$siteName};
+				#print $siteName."\t$P[$i]\t$SITENAME_maxSignal{$siteName}\t".$tempval."\n";
+				$siteOut2 .= "\t$tempval";
+			}
+			else
+			{
+				$siteOut2 .= "\t0";
+			}
+			}
+		
+		}
+	
+	print SITE_DATA "$siteName\t$siteName\t$chrID\t$bp1\t$bp2\t$SITENAME_expressed{$siteName}\t".($bp2-$bp1)."\n";
+	
+} close IN;
+close COUNTS; close SITE_DATA; 
+
+print CELL_DATA "cells\ttimepoint\tnum_genes_expressed\n";
+#might need to modify this to be more general than "timepoints"
+print DIMS_DATA "dimension_1\tdimension_2\n";
+
+for ($i = 0; $i < @H; $i++) {
+	if ((defined $CELLID_annot{$H[$i]}) && defined ($CELLID_DIMS{$H[$i]})) {
+		print DIMS_DATA  join("\t",@{$CELLID_DIMS{$H[$i]}})."\n";
+		print CELL_DATA "$H[$i]\t$H[$i]\t$CELLID_annot{$H[$i]}\t$CELLID_expressed{$H[$i]}\n";
+	}
+} close CELL_DATA; close DIMS_DATA;
+}
+1;
