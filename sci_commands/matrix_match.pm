03b8765761573985dc2a27876f55b31ddd4cfa3f
acadey <adey@ohsu.edu>
2018-07-16 10:15:12 -0700

    matrix match

---
+++ sci_commands/matrix_match.pm
@@ -0,0 +1,85 @@
+package sci_commands::matrix_match;
+
+use sci_utils::general;
+use Getopt::Std; %opt = ();
+use Exporter "import";
+@EXPORT = ("matrix_match");
+
+sub matrix_match {
+
+@ARGV = @_;
+
+# Defaults
+getopts("O:", \%opt);
+
+$die2 = "
+scitools matrix-match [options] [counts matrix 1] [counts matrix 2]
+   or    match-matrix
+
+Term frequency normalization of matrix
+
+Options:
+   -O   [STR]   Output prefix (default is [mat1_vs_mat2])
+
+";
+
+if (!defined $ARGV[1]) {die $die2};
+if (!defined $opt{'O'}) {
+	$opt{'O'} = $ARGV[0];
+	$opt{'O'} =~ s/\.matrix$//;
+	$opt{'O'} .= "_vs_".$ARGV[1];
+	$opt{'O'} =~ s/\.matrix$//;
+}
+
+open M1, "$ARGV[0]";
+$h1 = <M1>; chomp $h1; @H1 = split(/\t/, $h1);
+while ($l = <M1>) {
+	chomp $l;
+	@P = split(/\t/, $l);
+	$siteID = shift(@P);
+	@{$SITEID_row{$siteID}} = @P;
+} close M1;
+
+open M2, "$ARGV[1]";
+$h2 = <M2>; chomp $h2; @H2 = split(/\t/, $h2);
+for ($m1 = 0; $m1 < @H1; $m1++) {
+	for ($m2 = 0; $m2 < @H2; $m2++) {
+		$M1_M2_dist{$m1}{$m2} = 0;
+	}
+}
+while ($l = <M2>) {
+	chomp $l;
+	@P = split(/\t/, $l);
+	$siteID = shift(@P);
+	if (defined $SITEID_row{$siteID}[0]) {
+		for ($m1 = 0; $m1 < @H1; $m1++) {
+			for ($m2 = 0; $m2 < @H2; $m2++) {
+				$M1_M2_dist{$m1}{$m2} += abs($P[$m2]-$SITEID_row{$siteID}[$m1]);
+			}
+		}
+		$siteCT++;
+	}
+} close M2;
+
+open D, ">$opt{'O'}.all_dist.txt";
+open A, ">$opt{'O'}.best_match.annot";
+open R, ">$opt{'O'}.best_match_rev.annot";
+
+for ($m1 = 0; $m1 < @H1; $m1++) {
+	$cellID1 = $H1[$m1];
+	print D "$cellID1";
+	$win = "null";
+	foreach $m2 (sort {$M1_M2_dist{$m1}{$a}<=>$M1_M2_dist{$m1}{$b}} keys %{$M1_M2_dist{$m1}}) {
+		if ($win eq "null") {$win = $H2[$m2]};
+		$av_d = sprintf("%.3f", $M1_M2_dist{$m1}{$m2}/$siteCT);
+		print D "\t$av_d";
+	}
+	print D "\n";
+	print A "$cellID1\t$win\n";
+	print R "$win\t$cellID1\n";
+}
+
+close D; close A; close R;
+
+}
+1;
