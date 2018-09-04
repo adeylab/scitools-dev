0bfa7ed3293111a226557ad365b7caff3ab47961
Ryan Mulqueen <mulqueenr@gmail.com>
2018-09-04 14:25:45 -0700

    added explained variance plot to irlba

--- sci_commands/matrix_irlba.pm
+++ sci_commands/matrix_irlba.pm
@@ -35,9 +35,19 @@ if (defined $opt{'R'}) {$Rscript = $opt{'R'}};
 open R, ">$opt{'O'}.irlba.r";
 print R "
 library(irlba)
+library(ggplot2)
 IN<-as.matrix(read.table(\"$ARGV[0]\"))
 I<-irlba(IN, $dims)
 rownames(I\$v)<-colnames(IN)
+
+var<-as.data.frame(I\$d,ncol=1)
+colnames(var)<-c(\"d\")
+var\$var_names<-paste0(\"PC_\",row.names(var))
+var\$var_names<-factor(var\$var_names,levels=var\$var_names)
+
+var\$variance<-prop.table(var\$d^2)
+ggplot()+geom_histogram(aes(x=var\$var_names,y=var\$variance),stat=\"identity\")+xlab(\"PC\")+ylab(\"Variance Explained\")+theme_minimal()+theme(axis.text.x = element_text(angle = 90, hjust = 1))
+ggsave(\"$opt{'O'}.irlba_variance.png\")
 
 df<-as.data.frame(I\$v)
 PCA_colnames<-c()
