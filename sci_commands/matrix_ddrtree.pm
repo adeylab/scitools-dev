5a97218912a808eb1fc9509aa6d98bce50f65e0b
torkencz <thoor444@gmail.com>
2018-09-04 16:39:35 -0700

    define plot_cell_traj for precise write out

--- sci_commands/matrix_ddrtree.pm
+++ sci_commands/matrix_ddrtree.pm
@@ -262,6 +262,180 @@ ggsave(plot=p,filename=\"$opt{'O'}.timepoint_plot.png\",width=5,height=4,dpi=900
 ggsave(plot=p,filename=\"$opt{'O'}.timepoint_plot.pdf\",width=5,height=4);
 
 
+#this is to save the dims
+
+
+
+plot_cell_trajectory2<-function (cds, x = 1, y = 2, color_by = \"State\", show_tree = TRUE, 
+    show_backbone = TRUE, backbone_color = \"black\", markers = NULL, 
+    use_color_gradient = FALSE, markers_linear = FALSE, show_cell_names = FALSE, 
+    show_state_number = FALSE, cell_size = 1.5, cell_link_size = 0.75, 
+    cell_name_size = 2, state_number_size = 2.9, show_branch_points = TRUE, 
+    theta = 0, ...) 
+{
+    gene_short_name <- NA
+    sample_name <- NA
+    sample_state <- pData(cds)$State
+    data_dim_1 <- NA
+    data_dim_2 <- NA
+    lib_info_with_pseudo <- pData(cds)
+    if (is.null(cds\@dim_reduce_type)) {
+        stop(\"Error: dimensionality not yet reduced. Please call reduceDimension() before calling this function.\")
+    }
+    if (cds\@dim_reduce_type == \"ICA\") {
+        reduced_dim_coords <- reducedDimS(cds)
+    }
+    else if (cds@dim_reduce_type %in% c(\"simplePPT\", \"DDRTree\")) {
+        reduced_dim_coords <- reducedDimK(cds)
+    }
+    else {
+        stop(\"Error: unrecognized dimensionality reduction method.\")
+    }
+    ica_space_df <- data.frame(Matrix::t(reduced_dim_coords[c(x, 
+        y), ]))
+    colnames(ica_space_df) <- c(\"prin_graph_dim_1\", \"prin_graph_dim_2\")
+    ica_space_df$sample_name <- row.names(ica_space_df)
+    ica_space_df$sample_state <- row.names(ica_space_df)
+    dp_mst <- minSpanningTree(cds)
+    if (is.null(dp_mst)) {
+        stop(\"You must first call orderCells() before using this function\")
+    }
+    edge_list <- as.data.frame(get.edgelist(dp_mst))
+    colnames(edge_list) <- c(\"source\", \"target\")
+    edge_df <- merge(ica_space_df, edge_list, by.x = \"sample_name\", 
+        by.y = \"source\", all = TRUE)
+    edge_df <- plyr::rename(edge_df, c(prin_graph_dim_1 = \"source_prin_graph_dim_1\", 
+        prin_graph_dim_2 = \"source_prin_graph_dim_2\"))
+    edge_df <- merge(edge_df, ica_space_df[, c(\"sample_name\", 
+        \"prin_graph_dim_1\", \"prin_graph_dim_2\")], by.x = \"target\", 
+        by.y = \"sample_name\", all = TRUE)
+    edge_df <- plyr::rename(edge_df, c(prin_graph_dim_1 = \"target_prin_graph_dim_1\", 
+        prin_graph_dim_2 = \"target_prin_graph_dim_2\"))
+    S_matrix <- reducedDimS(cds)
+    data_df <- data.frame(t(S_matrix[c(x, y), ]))
+    data_df <- cbind(data_df, sample_state)
+    colnames(data_df) <- c(\"data_dim_1", \"data_dim_2\")
+    data_df$sample_name <- row.names(data_df)
+    data_df <- merge(data_df, lib_info_with_pseudo, by.x = \"sample_name\", 
+        by.y = \"row.names\")
+    return_rotation_mat <- function(theta) {
+        theta <- theta/180 * pi
+        matrix(c(cos(theta), sin(theta), -sin(theta), cos(theta)), 
+            nrow = 2)
+    }
+    tmp <- return_rotation_mat(theta) %*% t(as.matrix(data_df[, 
+        c(2, 3)]))
+    data_df$data_dim_1 <- tmp[1, ]
+    data_df$data_dim_2 <- tmp[2, ]
+    tmp <- return_rotation_mat(theta = theta) %*% t(as.matrix(edge_df[, 
+        c(\"source_prin_graph_dim_1\", \"source_prin_graph_dim_2\")]))
+    edge_df$source_prin_graph_dim_1 <- tmp[1, ]
+    edge_df$source_prin_graph_dim_2 <- tmp[2, ]
+    tmp <- return_rotation_mat(theta) %*% t(as.matrix(edge_df[, 
+        c(\"target_prin_graph_dim_1\", \"target_prin_graph_dim_2\")]))
+    edge_df$target_prin_graph_dim_1 <- tmp[1, ]
+    edge_df$target_prin_graph_dim_2 <- tmp[2, ]
+    markers_exprs <- NULL
+    if (is.null(markers) == FALSE) {
+        markers_fData <- subset(fData(cds), gene_short_name %in% 
+            markers)
+        if (nrow(markers_fData) >= 1) {
+            markers_exprs <- reshape2::melt(as.matrix(exprs(cds[row.names(markers_fData), 
+                ])))
+            colnames(markers_exprs)[1:2] <- c(\"feature_id\", \"cell_id\")
+            markers_exprs <- merge(markers_exprs, markers_fData, 
+                by.x = \"feature_id\", by.y = \"row.names\")
+            markers_exprs$feature_label <- as.character(markers_exprs$gene_short_name)
+            markers_exprs$feature_label[is.na(markers_exprs$feature_label)] <- markers_exprs$Var1
+        }
+    }
+    if (is.null(markers_exprs) == FALSE && nrow(markers_exprs) > 
+        0) {
+        data_df <- merge(data_df, markers_exprs, by.x = \"sample_name\", 
+            by.y = \"cell_id\")
+        if (use_color_gradient) {
+            if (markers_linear) {
+                g <- ggplot(data = data_df, aes(x = data_dim_1, 
+                  y = data_dim_2)) + geom_point(aes(color = value), 
+                  size = I(cell_size), na.rm = TRUE) + scale_color_viridis(name = paste0(\"value\"), 
+                  ...) + facet_wrap(~feature_label)
+            }
+            else {
+                g <- ggplot(data = data_df, aes(x = data_dim_1, 
+                  y = data_dim_2)) + geom_point(aes(color = log10(value + 
+                  0.1)), size = I(cell_size), na.rm = TRUE) + 
+                  scale_color_viridis(name = paste0(\"log10(value + 0.1)\"), 
+                    ...) + facet_wrap(~feature_label)
+            }
+        }
+        else {
+            if (markers_linear) {
+                g <- ggplot(data = data_df, aes(x = data_dim_1, 
+                  y = data_dim_2, size = (value * 0.1))) + facet_wrap(~feature_label)
+            }
+            else {
+                g <- ggplot(data = data_df, aes(x = data_dim_1, 
+                  y = data_dim_2, size = log10(value + 0.1))) + 
+                  facet_wrap(~feature_label)
+            }
+        }
+    }
+    else {
+        g <- ggplot(data = data_df, aes(x = data_dim_1, y = data_dim_2))
+    }
+    if (show_tree) {
+        g <- g + geom_segment(aes_string(x = \"source_prin_graph_dim_1\", 
+            y = \"source_prin_graph_dim_2\", xend = \"target_prin_graph_dim_1\", 
+            yend = \"target_prin_graph_dim_2\"), size = cell_link_size, 
+            linetype = \"solid\", na.rm = TRUE, data = edge_df)
+    }
+    if (is.null(markers_exprs) == FALSE && nrow(markers_exprs) > 
+        0) {
+        if (use_color_gradient) {
+        }
+        else {
+            g <- g + geom_point(aes_string(color = color_by), 
+                na.rm = TRUE)
+        }
+    }
+    else {
+        if (use_color_gradient) {
+        }
+        else {
+            g <- g + geom_point(aes_string(color = color_by), 
+                size = I(cell_size), na.rm = TRUE)
+        }
+    }
+    if (show_branch_points && cds@dim_reduce_type == \"DDRTree\") {
+        mst_branch_nodes <- cds@auxOrderingData[[cds@dim_reduce_type]]$branch_points
+        branch_point_df <- subset(edge_df, sample_name %in% mst_branch_nodes)[, 
+            c(\"sample_name\", \"source_prin_graph_dim_1\", \"source_prin_graph_dim_2\")]
+        branch_point_df$branch_point_idx <- match(branch_point_df$sample_name, 
+            mst_branch_nodes)
+        branch_point_df <- branch_point_df[!duplicated(branch_point_df$branch_point_idx), 
+            ]
+        g <- g + geom_point(aes_string(x = \"source_prin_graph_dim_1\", 
+            y = \"source_prin_graph_dim_2\"), size = 5, na.rm = TRUE, 
+            data = branch_point_df) + geom_text(aes_string(x = \"source_prin_graph_dim_1\", 
+            y = \"source_prin_graph_dim_2\", label = \"branch_point_idx\"), 
+            size = 4, color = \"white\", na.rm = TRUE, data = branch_point_df)
+    }
+    if (show_cell_names) {
+        g <- g + geom_text(aes(label = sample_name), size = cell_name_size)
+    }
+    if (show_state_number) {
+        g <- g + geom_text(aes(label = sample_state), size = state_number_size)
+    }
+    g <- g + xlab(paste(\"Component\", x)) + 
+        ylab(paste(\"Component\", y)) + theme(legend.position = \"top\", 
+        legend.key.height = grid::unit(0.35, \"in\")) + theme(legend.key = element_blank()) + 
+        theme(panel.background = element_rect(fill = \"white\"))
+    g
+    write.table(data_df,file=\"save.dims.txt\",col.names=T,row.names=F,quote=F,sep=\"\\t\")
+}
+
+
+
 
 
 #add stuff here if you want to reroot agg_cds <- orderCells(agg_cds, root_state = \"D\")
@@ -275,7 +449,7 @@ pData(input_cds)\$State <- pData(agg_cds)[colnames(input_cds),]\$State
 
 
 # writing out pseudotime etc so you can recreate everything
-write.table(as.matrix(pData(agg_cds)),file=\"./$opt{'O'}_ddrt_aggragated_cells.txt\",col.names=TRUE,row.names=FALSE,sep=\"\\t\",quote=FALSE)
+write.table(as.matrix(pData(agg_cds)),file=\"./$opt{'O'}_ddrt_aggragated_cells.txt\",col.names=TRUE,row.names=TRUE,sep=\"\\t\",quote=FALSE)
 write.table(as.matrix(Biobase::exprs(agg_cds)),file=\"./$opt{'O'}_ddrt_aggragated_cells_norm_count.txt\",col.names=TRUE,row.names=TRUE,sep=\"\\t\",quote=FALSE)
 write.table(as.matrix(fData(agg_cds)),file=\"./$opt{'O'}_ddrt_aggragated_features.txt\",col.names=TRUE,row.names=FALSE,sep=\"\\t\",quote=FALSE)
 ";
