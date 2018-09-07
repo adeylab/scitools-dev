33ad690921e879657c218d379124103fd5f96d0a
Ryan Mulqueen <mulqueenr@gmail.com>
2018-09-07 11:43:31 -0700

    cisTopic --user defined topic numbers added

--- sci_commands/matrix_cistopic.pm
+++ sci_commands/matrix_cistopic.pm
@@ -37,9 +37,8 @@ Options:
    -r   [INT]   Number of nonZero sites per row (peak) to retain (def = 1)
    -n 	[INT] 	Number of cores for parallel processing. (def=1)
    -T    [INT]    User defined number of Topics to use. 
-                  If unspecified: will generate 15, 20, 25, 30, 50, and 65 Topics, 
+                  If unspecified: will generate 15, 20, 25, 30, 50, 65, 100 Topics, 
                   and use log-liklihood estimators to select the best.
-   -D   [INT]   Max dimensions to compute (def = $dims)
    -X           Retain intermediate files (def = delete)
    -R   [STR]   Rscript call (def = $Rscript)
 
@@ -70,24 +69,26 @@ if (!defined $opt{'T'}) {
 print R "
 #Run Multiple models with different topic numbers. Optimal topic number is generally slightly bigger than the potential cell states in the data set
 cisTopicObject <- runModels(cisTopicObject, topic=c(15, 20, 25, 30, 50, 65, 100), seed=2018, nCores=$opt{'n'}, burnin = 250, iterations = 300)
-saveRDS(cisTopicObject,\"$opt{'O'}.cistopicObject.rds\")
+#saveRDS(cisTopicObject,\"$opt{'O'}.cistopicObject.rds\")
 
 #Future update:Plot model log likelihood (P(D|T)) at the last iteration
 pdf(file=\"$opt{'O'}.cistopic.modelselection.pdf\")
 cisTopicObject <- selectModel(cisTopicObject)
 logLikelihoodByIter(cisTopicObject)
 dev.off()
+modelMat <- scale(cisTopicObject\@selected.model\$document_expects, center = TRUE, scale = TRUE)
+
 ";
 } else {
 print R "
 cisTopicObject <- runModels(cisTopicObject, topic=$opt{'T'}, seed=2018, nCores=$opt{'n'}, burnin = 250, iterations = 300)
-cisTopicObject <- selectModel(cisTopicObject)
+modelMat<-scale(cisTopicObject@models$document_expects,center=TRUE,scale=TRUE)
+
 ";
 }
 
 print R "
 #Print out cisTopic Matrix#
-modelMat <- scale(cisTopicObject\@selected.model\$document_expects, center = TRUE, scale = TRUE)
 tModelmat<-as.data.frame(t(modelMat))
 Modeldf<-as.data.frame(modelMat)
 rownames(tModelmat)<-cisTopicObject\@cell.names
