cad1aa7e85b84d6f8aea59f960114dfbb798c9dc
Ryan Mulqueen <mulqueenr@gmail.com>
2018-10-02 17:11:24 -0700

    WIP Updates to plot_plotly

--- sci_commands/plot_plotly.pm
+++ sci_commands/plot_plotly.pm
@@ -16,7 +16,7 @@ Currently accepted files:
 Annotation files:                            Will generate a legend with each annotation.
 Monocle monocle3_cells.txt:                  Will plot pseudotime value over cells as continuous color scale.
 Value files:                                 Will plot values over cells as continuous color scale.
-ChromVar deviation_scores.txt                Will plot scores over cells as continuous color scale.
+ChromVar deviation_scores.matrix             Will plot scores over cells as continuous color scale.
                                              Limited to 10 specified transcription factors.
 Monocle monocle3_segmentvectors.txt output:  Will plot cds_monocle cell trajectories.
 
@@ -26,7 +26,7 @@ Options:
    -A   [STR]   Comma-separated list of .annot files. Will generate a legend with each annotation.
    -V   [STR]   Comma-separated list of .val files. Will generate a legend item for each value.
    -p   [STR]   scitools cds_monocle generated monocle3_cells.txt file. Will generate a legend item for pseudotime.
-   -M   [STR]   scitools chromvar generated deviation_scores.txt file. Will read in transcription factor deviation scores file.
+   -M   [STR]   scitools chromvar generated deviation_scores.matrix file. Will read in transcription factor deviation scores file.
                   If specified, -m must also be specified.
    -m   [STR]   comma separated list of transcription factors. CAN ONLY BE LENGTH 10 OR LESS.   
                   If specified, -M must also be specified.
@@ -47,7 +47,11 @@ if (ncol(dims)==2){
    colnames(dims)<-c(\"cellID\",\"x\",\"y\")
 } else {
    colnames(dims)<-c(\"cellID\",\"x\",\"y\",\"z\")
-}";
+}
+write(paste(\"Read in dims file:\",\"$ARGV[0]\",sep=\"\\t\"),stderr())
+write(paste(\"Dimensions Detected:\",as.character(ncol(dims)-1),sep=\"\\t\"),stderr())
+
+";
 $alternative_color_files=0;
 
 #Read in given annotation files
@@ -70,7 +74,7 @@ if (defined $opt{'A'}) {
    #generate a new data frame for each file read in
    assign(paste0(\"col_annot_\",i_name),working_df)
 
-   write(paste(\"Read in file:\",i,sep=\"\\t\"),stderr())
+   write(paste(\"Read in annot file:\",i,sep=\"\\t\"),stderr())
    }";
 };
 
@@ -94,7 +98,7 @@ if (defined $opt{'V'}) {
    #generate a new data frame for each file read in
    assign(paste0(\"col_val_\",i_name),working_df)
    
-   write(paste(\"Read in file:\",i,sep=\"\\t\"),stderr())
+   write(paste(\"Read in value file:\",i,sep=\"\\t\"),stderr())
    }";
 };
 
@@ -103,12 +107,13 @@ if (defined $opt{'p'}) {
 
    $alternative_color_files=1;
    print R "
-   if(!endsWith(\"$opt{'p'}\"),\"monocle3_cells.txt\")
+   if(!grepl(\"monocle3_cells.txt\$\", \"$opt{'p'}\")) {
       stop(\"Error: ensure your -p option is file named monocle3_cells.txt\")
+   }
    col_m_file<-read.table{\"$opt{'p'}\",header=T}
    col_m_file\$cellID<-row.names(col_m_file)
    col_m_file<-col_m_file[,c(18,17)]
-   write(paste(\"Read in file:\",\"$opt{'p'}\",sep=\"\\t\"),stderr())
+   write(paste(\"Read in pseudotime file:\",\"$opt{'p'}\",sep=\"\\t\"),stderr())
 
    ";
 };
@@ -117,11 +122,12 @@ if (defined $opt{'p'}) {
 if (defined $opt{'C'}) {
    $alternative_color_files=1;
    print R "
-   if(!endsWith(\"$opt{'C'}\"),\"complexity.txt\")
+   if(!grepl(\"complexity.txt\$\", \"$opt{'C'}\")) {
       stop(\"Error: ensure your -C option is a complexity file.\")
+   }
    col_c_file<-read.table(\"$opt{'C'}\",header=F, row.names=1)
    colnames(col_c_file)<-c(\"cellID\",\"total_reads\",\"uniq_reads\",\"percent_uniq\")
-   write(paste(\"Read in file:\",\"$opt{'C'}\",sep=\"\\t\"),stderr())
+   write(paste(\"Read in complexity file:\",\"$opt{'C'}\",sep=\"\\t\"),stderr())
 
    ";
 };
@@ -130,14 +136,16 @@ if (defined $opt{'C'}) {
 if (defined $opt{'M'} && defined $opt{'m'}) {
    $alternative_color_files=1;
    @tf_list = split(/,/,$opt{'m'});
-   foreach (@val_list) {$_ = "'$_'";}
+   foreach (@tf_list) {$_ = "'$_'";}
    $opt{'m'}=join(', ', @tf_list );
    $alternative_color_files=1;
    print R "
-   if(!endsWith(\"$opt{'M'}\"),\"deviation_scores.txt\"),
-      stop(\"Error: ensure your -M option is a deviation_scores.txt file\")
-   if(length($opt{'M'})>10),
+   if(!grepl(\"deviation_scores.matrix\$\", \"$opt{'M'}\")) {
+      stop(\"Error: ensure your -M option is a deviation_scores.matrix file\")
+   }
+   if(length(c($opt{'m'}))>10){
       stop(\"Error: Limit your transcription factor list to 10.\")
+   }
    M_file<-read.table(\"$opt{'M'}\",header=T)
 
    tf_list<-c($opt{'m'})
@@ -146,13 +154,14 @@ if (defined $opt{'M'} && defined $opt{'m'}) {
    check_each <- rownames(M_file)[grepl(x,rownames(M_file),ignore.case=TRUE)]
    out<-c(out,check_each)
    }
-   write(paste(\"Found transcription factor matches:\",paste(out,sep=\"\\n\"),sep=\"\\n\"),stderr())
+   write(\"Found transcription factor matches:\",stderr())
+   write(paste(out,sep=\"\\n\"),stderr())
 
 
    col_M_file<-subset(M_file,rownames(M_file) \%in\% out)
    col_M_file<-data.frame(t(col_M_file))
    col_M_file\$cellID<-rownames(col_M_file)
-   write(paste(\"Read in file:\",\"$opt{'M'}\",sep=\"\\t\"),stderr())
+   write(paste(\"Read in chromvar file:\",\"$opt{'M'}\",sep=\"\\t\"),stderr())
 
    ";
 };
@@ -161,11 +170,12 @@ if (defined $opt{'M'} && defined $opt{'m'}) {
 if (defined $opt{'B'}) {
    $alternative_color_files=1;
    print R "
-   if(!endsWith(\"$opt{'B'}\"),\"monocle3_segmentvectors.txt\")
+   if(!grepl(\"monocle3_segmentvectors.txt\$\", \"$opt{'B'}\")) {
       stop(\"Error: ensure your -B option is file named monocle3_segmentvectors.txt\")
+   }
    b_file<-read.table(\"$opt{'B'}\",header=T)
    b_file\$line_segment<-as.factor(b_file\$line_segment)
-   write(paste(\"Read in file:\",\"$opt{'B'}\",sep=\"\\t\"),stderr())
+   write(paste(\"Read in monocle branchpoint file:\",\"$opt{'B'}\",sep=\"\\t\"),stderr())
 
    ";
 };
@@ -187,25 +197,35 @@ print R "
 #make a plotly loop to go through files.
 #Generate 3D Plot
 if (\"z\" %in% colnames(dims)) {
-p<-plot_ly(data=dims,x = ~x, y = ~y, z = ~z,marker=list(size=0.6,opacity=0.7),hovertext=~cellID)
-for (i in 5:ncol(dims)) {
-   p<-add_trace(p,color=~dims[,i])
+p<-plot_ly(type=\"scatter3d\",mode=\"markers\")
+p<- p %>% add_trace(p,data=dims,x = ~x, y = ~y, z = ~z,marker=list(size=1,opacity=0.7),color=~dims[,5],hovertext=~cellID)
+for (i in 6:ncol(dims)) {
+   if (sapply(dims[i],is.factor)) {
+      p<- p %>% add_trace(p,data=dims,x = ~x, y = ~y, z = ~z,marker=list(size=1,opacity=0.7),color=~dims[,i],hovertext=~cellID,visible=\"legendonly\")
+      } else {
+      p<- p %>% add_trace(p,data=dims,x = ~x, y = ~y, z = ~z,marker=list(size=1,opacity=0.7),color=~dims[,i],hovertext=~cellID,visible=\"legendonly\")
+      }
    }
 if (exists(\"b_file\")) {
-   add_paths(p,data=b_file, split=~line_segment,x=~prin_graph_dim_1,y=~prin_graph_dim_2,z=~prin_graph_dim_3,line=list(width=3,color=\"black\"),showlegend=FALSE)
+   p<- p %>% add_paths(p,data=b_file, split=~line_segment,x=~prin_graph_dim_1,y=~prin_graph_dim_2,z=~prin_graph_dim_3,line=list(width=3,color=\'rgb(0, 0, 0)\'),showlegend=FALSE)
 }
-p %>% layout(scene = list(xaxis = list(title = \'X\'),yaxis = list(title = \'Y\'), zaxis = list(title = \'Z\')))
+p<-p %>% layout(scene = list(xaxis = list(title = \'X\'),yaxis = list(title = \'Y\'), zaxis = list(title = \'Z\')))
 
 } else {
 #Generate 2D Plot
-p<-plot_ly(data=dims,x = ~x, y = ~y, z = ~z,marker=list(size=0.6,opacity=0.7),hovertext=~cellID)
-for (i in 4:ncol(dims)) {
-   p<-add_trace(p,color=~dims[,i])
+p<-plot_ly(data=dims,x = ~x, y = ~y, marker=list(size=1,opacity=0.7),hovertext=~cellID)
+p<- p %>% add_trace(p,data=dims,x = ~x, y = ~y, marker=list(size=1,opacity=0.7),color=~dims[,4],hovertext=~cellID)
+for (i in 5:ncol(dims)) {
+   if (sapply(dims[i],is.factor)) {
+      p<- p %>% add_trace(p,data=dims,x = ~x, y = ~y, marker=list(size=1,opacity=0.7),color=~dims[,i],hovertext=~cellID,visible=\"legendonly\")
+      } else {
+      p<- p %>% add_trace(p,data=dims,x = ~x, y = ~y, marker=list(size=1,opacity=0.7),color=~dims[,i],hovertext=~cellID,visible=\"legendonly\")
+      }
    }
 if (is.data.frame(b_file)) {
-   add_paths(p,data=b_file, split=~line_segment,x=~prin_graph_dim_1,y=~prin_graph_dim_2,line=list(width=3,color=\"black\"),showlegend=FALSE)
+   p <- p %>% add_paths(p,data=b_file, split=~line_segment,x=~prin_graph_dim_1,y=~prin_graph_dim_2,line=list(width=3,color=\'rgb(0, 0, 0)\'),showlegend=FALSE)
 }
-p %>% layout(scene = list(xaxis = list(title = \'X\'),yaxis = list(title = \'Y\')))
+p<-p %>% layout(scene = list(xaxis = list(title = \'X\'),yaxis = list(title = \'Y\')))
 }
 htmlwidgets::saveWidget(widget=p,\"$opt{'O'}.html\", selfcontained = FALSE)
 
