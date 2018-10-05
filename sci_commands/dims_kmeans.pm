229b8a86d1cccd06519ab08ce892ddfae5af68d7
Ryan Mulqueen <mulqueenr@gmail.com>
2018-10-05 14:49:31 -0700

    kmeans_fix and push for plotting

--- sci_commands/dims_kmeans.pm
+++ sci_commands/dims_kmeans.pm
@@ -112,7 +112,7 @@ par(
 plot(k, type=\'b\', avg_sil, xlab=\'Number of clusters\', ylab=\'Average Silhouette Scores\', frame=FALSE)
 dev.off()
 
-k_row<-$opt{'K'}/4
+k_row<-as.integer($opt{'K'}/4)
 png(  \"$opt{'O'}.silhouette_widths.K$opt{'K'}.png\",)
 par(mfrow=c(k_row,4))
 for (i in k){
