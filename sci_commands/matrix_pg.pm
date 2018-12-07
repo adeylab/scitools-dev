f527e7177e5deb58d831acfd1fd80d825785424c
Ryan Mulqueen <mulqueenr@gmail.com>
2018-12-07 15:38:51 -0800

    Phenogram matrix_pg call now takes k nearest neighbors and uses R implementation

--- sci_commands/matrix_pg.pm
+++ sci_commands/matrix_pg.pm
@@ -10,7 +10,7 @@ sub matrix_pg {
 @ARGV = @_;
 
 
-getopts("O:XP:", \%opt);
+getopts("O:XR:k:", \%opt);
 
 $die2 = "
 scitools matrix-pg [options] [input matrix or dims] 
@@ -19,59 +19,53 @@ This scripts uses Louvain method to find clusters of cells based on provided dim
 
 Options:
    -O   [STR]   Output prefix (default is [input].pg.annot)
-   -X           Retain intermediate files (def = delete)
-   -P   [STR]   python script call (def = $Pscript)
-
-Note: Requires numpy and phenograph python packages
+   -k	[INT]	Number of nearest-neighbor cells to use for clustering. (Default:50)
+   -X           Remove intermediate files (def = Keep if unflagged)
+   -R   [STR]   R script call (def = $Rscript)
 
 ";
 
 
 if (!defined $ARGV[0]) {die $die2};
+if (!defined $opt{'k'}) {$opt{'k'}=50};
 if (!defined $opt{'O'}) {$opt{'O'} = $ARGV[0]};
 $opt{'O'} =~ s/\.matrix$//;
 $opt{'O'} =~ s/\.dims$//;
 if (defined $opt{'P'}) {$Pscript = $opt{'P'}};
 read_matrix($ARGV[0]);
 
-open OUT, ">$opt{'O'}.pg.py";
+open OUT, ">$opt{'O'}.phenograph.R";
 print OUT"
-import phenograph
-import numpy
+library(Rphenograph)
 ";
+
+
 if ($ARGV[0] =~ /\.dims/) {
 print OUT "
-data_matrix=numpy.loadtxt(\"$ARGV[0]\",skiprows=1,usecols=range(1,$matrix_colNum))\n";
+dat_matrix<-as.data.frame(read.table(\"$ARGV[0]\",header=T)
+";
 } else {
-print OUT "ori_matrix=numpy.loadtxt(\"$ARGV[0]\",skiprows=1,usecols=range(1,$matrix_colNum))
-data_matrix=ori_matrix.transpose()\n";
+print OUT "
+dat_matrix<-as.data.frame(t(read.table(\"$ARGV[0]\",header=T))
+";
 }
 
 
-#detext communities. Graph, later
+#detect communities by given k value.
 print OUT "
+cellID<-row.names(dat_matrix)
 
-communities, graph, Q = phenograph.cluster(data_matrix)
-numpy.savetxt(\"$opt{'O'}.temp.pg.annot\",communities.astype(int), fmt='%i',delimiter=\"\\t\")
+dat_pg<-Rphenograph(dat_matrix,k=$opt{'k'})
+pg_clusterID<-paste(\"pg_\",membership(dat_pg[[2]]),sep=\"\")
+dat_out<-do.call(rbind, Map(data.frame, A=cellID, B=pg_clusterID))
+
+write.table(dat_out,file=\"$opt{'O'}.k${'k'}.pg.annot\",col.names=F,row.names=F,quote=F,sep=\"\\t\")
 ";
 close OUT;
-system("$Pscript $opt{'O'}.pg.py");
+system("$Rscript $opt{'O'}.pg.R");
 
-open OUT, ">$opt{'O'}.pg.annot";
-$counter=0;
-open IN, "$opt{'O'}.temp.pg.annot"; 
-while($l=<IN>)
-{
-$l =~ s/"//g;   
-print OUT $MATRIX_COLNAMES[$counter]."\tCluster_".$l;
-$counter++;
-}
-close(IN);
-close OUT;
-
-
-if (!defined $opt{'X'}) {
-	system("rm -f $opt{'O'}.temp.pg.annot $opt{'O'}.pg.py");
+if (defined $opt{'X'}) {
+	system("rm -f $opt{'O'}.pg.R");
 }
 
 }
