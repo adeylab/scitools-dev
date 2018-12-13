1860333ac300a9b0e4ac6496eca5b9d0e6a56b5c
Ryan Mulqueen <mulqueenr@gmail.com>
2018-12-13 12:49:13 -0800

    cds_cicero now outputs a gene activity score matrix

--- sci_commands/cds_cicero.pm
+++ sci_commands/cds_cicero.pm
@@ -19,13 +19,21 @@ scitools cds-cicero [options] [directory containing cds files]
 Prior to running, ensure you have ran scitools matrix-makecds. 
 That function will convert matrix files into the CDS format that Cicero requires.
 Runs the current version of Cicero within a directory containing files for CDS format input. 
+Outputs:
+Tables describing
+ the coaccessibility of peaks:                          cicero_conns.txt
+ the membership peaks within CCANs:                     cicero.CCANS.txt
+RDS files for
+  the coaccessibility of peaks:                         cicero.conns.rds
+  the gene activity score matrix (in dgC matrix):       cicero.gene_activity_matrix.rds
+  the working cds file (after aggregation):             cicero.CDS.rds
 
 
 Options:
    -O    [STR]   Output Directory (default is [current working directory]/cicero_output)
    -R    [STR]   Rscript call (def = $Rscript)
    -G    [STR]   Genome to be used. Must be of list:
-                  human.hg38.genome,human.hg19.genome,human.hg38.genome,mouse.mm10.genome
+                  [human.hg38.genome,mouse.mm10.genome]
                   Default: human.hg38.genome
    -P 	 [INT] 	Distance (in bp) between Peaks for aggregation. (Default: 10000) 
    -p     [INT]   Give CCAN values to print seperatefd by commas (e.g.: 1,2,3,4), if given value \"ALL\" all will be printed out      
@@ -73,9 +81,9 @@ dimension_reduction<-cds_dims_data[2:ncol(cds_dims_data)]
 message(\"Setting up CDS matrix, binarized for Cicero.\")
 
 
-cds <- suppressWarnings(newCellDataSet(as.matrix(cds_counts_matrix), phenoData = sample_data, featureData = feature_data,expressionFamily = negbinomial.size(),lowerDetectionLimit = 0))
+cds <- suppressWarnings(newCellDataSet(as.matrix(cds_counts_matrix), phenoData = sample_data, featureData = feature_data,expressionFamily = VGAM::negbinomial.size(),lowerDetectionLimit = 0))
 
-cds\@expressionFamily <- binomialff()
+cds\@expressionFamily <- VGAM::binomialff()
 cds\@expressionFamily\@vfamily <- \"binomialff\"
 
 pData(cds)\$temp <- NULL
@@ -84,12 +92,16 @@ fData(cds)\$bp1 <- as.numeric(as.character(fData(cds)\$bp1))
 fData(cds)\$bp2 <- as.numeric(as.character(fData(cds)\$bp2))
 cds <- cds[order(fData(cds)\$chr, fData(cds)\$bp1),]
 
+#Running necessary preprocessing steps
 set.seed(2017)
 pData(cds)\$cells <- NULL
 cds <- aggregate_nearby_peaks(cds, distance = $opt{'P'})
 cds <- detectGenes(cds)
 cds <- estimateSizeFactors(cds)
 cds <- estimateDispersions(cds)
+
+#Spliting cds to a pre-aggregation data set
+input_cds<-cds
 
 message(\"Using Supplied Dims File.\")
 cds <- make_cicero_cds(cds, reduced_coordinates = dimension_reduction, k=$opt{'k'})
@@ -102,14 +114,16 @@ print R "
 
 human.hg38.genome<-read.table(\"/home/groups/oroaklab/refs/hg38/hg38.bedtools.genome\",header=F)
 conns <- run_cicero(cds, human.hg38.genome)
-
+gene_annotation<-read.table(\"/home/groups/oroaklab/refs/hg38/hg38.refGene.bed\",header=F)
+colnames(gene_annotation)<-c(\"chr\",\"start\",\"end\",\"gene\")
 ";
 } else {
 print R "
 
 data(\"$opt{'G'}\")
 conns <- run_cicero(cds, $opt{'G'})
-
+gene_annotation<-read.table(\"/home/groups/oroaklab/refs/mm10/mm10.RefGene.names.bed\",header=F)
+colnames(gene_annotation)<-c(\"chr\",\"start\",\"end\",\"gene\")
 ";
 }
 
@@ -126,6 +140,13 @@ head(CCAN_assigns)
 saveRDS(cds,\"$opt{'O'}/cicero.CDS.rds\")
 write.table(as.data.frame(conns), file=\"$opt{'O'}/cicero.output.txt\",quote=F,sep=\"\\t\",row.names=F,col.names=T)
 write.table(as.data.frame(CCAN_assigns), file=\"$opt{'O'}/cicero.CCANS.txt\",quote=F,sep=\"\\t\",row.names=F,col.names=F)
+
+message(\"Sample CCANs Output:\")
+input_cds <- annotate_cds_by_site(input_cds, gene_annotation)
+# generate unnormalized gene activity matrix
+unnorm_ga <- build_gene_activity_matrix(input_cds, conns)
+saveRDS(unnorm_ga,\"$opt{'O'}/cicero.gene_acitivity_matrix.rds\")
+write.table(as.data.frame(as.matrix(unnorm_ga)),file=\"$opt{'O'}/cicero.gene_activity_matrix.txt\",row.names=T,col.names=T,sep=\"\\t\",quote=F)
 ";
 
 close R;
