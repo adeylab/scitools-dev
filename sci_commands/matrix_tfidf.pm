14a04e0094c6f5dc301c8e5a568ca5917ae1574b
Andrew Adey <adey@ohsu.edu>
2019-02-04 19:55:36 -0800

    WIP spase support

--- sci_commands/matrix_tfidf.pm
+++ sci_commands/matrix_tfidf.pm
@@ -9,52 +9,65 @@ sub matrix_tfidf {
 
 @ARGV = @_;
 
-getopts("O:L:", \%opt);
+getopts("O:L:F:", \%opt);
 
 $die2 = "
-scitools matrix-tfidf [options] [counts matrix]
+scitools matrix-tfidf [options] [counts matrix or sparseMatrix values file]
    or    tfidf
 
 Options:
    -O   [STR]   Output prefix (default is [input].tfidf)
    -L   [BASE]  Log norm with base specified.
                 N for natural, def = no additional log norm
+   -F   [S/D]   Format to output: D = dense, S = sparse (def = input)
 
 ";
 
 if (!defined $ARGV[0]) {die $die2};
-if (!defined $opt{'O'}) {$opt{'O'} = $ARGV[0]; $opt{'O'} =~ s/\.matrix$//};
 
-read_matrix_stats($ARGV[0]);
+if ($ARGV[0] =~ /\.matrix/ || $ARGV[0] =~ /\.matrix\.gz/) {
 
-open IN, "$ARGV[0]";
-open OUT, ">$opt{'O'}.tfidf";
-$h = <IN>; print OUT "$h";
-while ($l = <IN>) {
-	chomp $l;
-	@P = split (/\t/, $l);
-	$rowID = shift(@P);
-	print OUT "$rowID";
-	for ($i = 0; $i < @P; $i++) {
-		if ($P[$i]>0) {
-			$tf = ($P[$i]/$MATRIX_CellID_signal{$MATRIX_COLNAMES[$i]});
-			$idf = (log(1+($matrix_colNum/($MATRIX_feature_signal{$rowID}+1))));
-			$raw_score = $tf*$idf;
-			if (defined $opt{'L'}) {
-				if ($opt{'L'} =~ /N/i) {
-					$raw_score = log($raw_score);
-				} else {
-					$raw_score = log($raw_score)/log($opt{'L'})
+	if (!defined $opt{'O'}) {$opt{'O'} = $ARGV[0]; $opt{'O'} =~ s/\.gz$//; $opt{'O'} =~ s/\.matrix$//};
+
+	read_matrix_stats($ARGV[0]);
+
+	open IN, "$ARGV[0]";
+	open OUT, ">$opt{'O'}.tfidf";
+	$h = <IN>; print OUT "$h";
+	while ($l = <IN>) {
+		chomp $l;
+		@P = split (/\t/, $l);
+		$rowID = shift(@P);
+		print OUT "$rowID";
+		for ($i = 0; $i < @P; $i++) {
+			if ($P[$i]>0) {
+				$tf = ($P[$i]/$MATRIX_CellID_signal{$MATRIX_COLNAMES[$i]});
+				$idf = (log(1+($matrix_colNum/($MATRIX_feature_signal{$rowID}+1))));
+				$raw_score = $tf*$idf;
+				if (defined $opt{'L'}) {
+					if ($opt{'L'} =~ /N/i) {
+						$raw_score = log($raw_score);
+					} else {
+						$raw_score = log($raw_score)/log($opt{'L'})
+					}
 				}
+				$score = sprintf("%.6f", $raw_score);
+			} else {
+				$score = "0";
 			}
-			$score = sprintf("%.6f", $raw_score);
-		} else {
-			$score = "0";
+			print OUT "\t$score";
 		}
-		print OUT "\t$score";
-	}
-	print OUT "\n";
-} close IN; close OUT;
+		print OUT "\n";
+	} close IN; close OUT;
+
+} elsif ($ARGV[0] =~ /sparseMatrix/) {
+	
+	if (!defined $opt{'O'}) {$opt{'O'} = $ARGV[0]; $opt{'O'} =~ s/\.gz$//; $opt{'O'} =~ s/\.sparseMatrix.values$//};
+	
+	############
+	die "ERROR: Sparse matrix support currently in progress!\n";
+	
+}
 
 }
 1;
