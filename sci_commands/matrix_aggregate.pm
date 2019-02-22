7b8e34c23e7b68acb3fbacbf912dff1871575d9e
Andrew Adey <adey@ohsu.edu>
2019-02-22 15:30:02 -0800

    complete matrix-aggregate rewrite

--- sci_commands/matrix_aggregate.pm
+++ sci_commands/matrix_aggregate.pm
@@ -9,275 +9,276 @@ sub matrix_aggregate {
 
 @ARGV = @_;
 use Getopt::Std; %opt = ();
-getopts("O:r:c:a:bmR", \%opt);
+getopts("O:a:o:tk:zL:W:", \%opt);
+
+# defaults
+$operations = "add";
+$constant = "rownum";
 
 $die2 = "
-scitools matrix-aggregate [options] [counts matrix] [annotation file]
+scitools matrix-aggregate [options] [matrix or sparseMatrix values file] [annotation file]
    or    aggregate-matrix
-
-Note: if a non counts matrix is provided, it will still sum the values.
-Support for comma-separated annotations (i.e. cell aggregation with
-oversampling) has been added.
 
 Options:
    -O   [STR]   Output prefix (default is [input annot].matrix)
-   -c   [STR]   File with list of CellIDs to retain
-   -r   [STR]   File with list of RowIDs to retain
-   -a   [STR]   Comma separated list of annotations to include
-   -b           Treat each cell as a binary value (resulting matrix is
-                  the number of cells at the peak. def = total counts)
-   -m           Instead aggregate by the annotations and print a mean
-                  and standard deviation for the aggregates
-                  (overrides -b)
-   -R 	[FLAG] 	Print and run an R script alternate version for large matrices.
-   				Development Note: R script alternative currently doesn't accept any options.
-   
+   -a   [STR]   Comma separated list of annotations to include, will also order output
+   -o   [STR]   Operation(s), comma separated list of the follwing (def = $operations)
+                  Each will be a seprate 
+                  add/a       adds values of merged cells
+                  mean/m      mean of merged cells
+                  stdev/s     standard deviation
+                  median/d    median of cells
+                  count/c     number of nonzero cells at the site
+   -t           Factor in term-frequency prior to performing operation
+                  (value of site / sum of all sites for the cell) * constant
+   -k   [STR]   Constant to multiply for term-frequency can be a number or 'rownum'
+                  def = $constant
+   -L   [STR]  Columns file for sparseMatrix (will try to auto-detect)
+   -W   [STR]  Rows file for sparseMatrix (will try to auto-detect)
+   -z           Gzip output
 ";
 
-if (defined $opt{'R'}) {
+if (!defined $ARGV[1]) {die $die2};
 
-if (!defined $opt{'O'}) {$opt{'O'} = $ARGV[1]; $opt{'O'} =~ s/\.matrix$//};
-if (!defined $ARGV[1]) {die $die2}
-
-open R, ">$opt{'O'}.matrix_aggregate.r";
-if (defined $opt{'b'})
-{
-print R "
-counts<-read.table(\"$ARGV[0]\")
-counts[counts > 0] = 1
-";
-}
-else
-{
-print R "
-counts<-read.table(\"$ARGV[0]\")
-";
-}
-print R "
-centroids<-read.table(\"$ARGV[1]\")
-
-centroid_df<-matrix(rowSums(counts[colnames(counts)\%in\%centroids[centroids\$V2==unique(centroids\$V2)[1],]\$V1]))
-
-for (i in unique(centroids\$V2)[2:length(unique(centroids\$V2))]){
-x<-rowSums(counts[colnames(counts)\%in\%centroids[centroids\$V2==i,]\$V1])
-centroid_df<-cbind(centroid_df,matrix(x))}
-
-centroid_df<-as.data.frame(centroid_df)
-colnames(centroid_df)<-unique(centroids\$V2)
-row.names(centroid_df)<-row.names(counts)
-
-
-";
-if (defined $opt{'b'})
-{
-print R "
-write.table(centroid_df,\"$opt{'O'}.aggregate.binary.R.matrix\",col.names=T,row.names=T,sep=\"\\t\",quote=F)
-";
-}
-else
-{
-print R "
-write.table(centroid_df,\"$opt{'O'}.aggregate.R.matrix\",col.names=T,row.names=T,sep=\"\\t\",quote=F)
-";
+if (!defined $opt{'O'}) {
+	$prefix = $ARGV[0];
+	$prefix =~ s/\.gz$//;
+	$prefix =~ s/\.matrix$//;
+	$prefix =~ s/\.sparseMatrix$//;
+	$afile = $ARGV[1];
+	$afile =~ s/\.annot$//;
+	$opt{'O'} = $prefix.".".$afile;
 }
 
-close R;
-system("$Rscript $opt{'O'}.matrix_aggregate.r");
-
-} else {
-#name output 
-if (!defined $opt{'O'}) {$opt{'O'} = $ARGV[1]; $opt{'O'} =~ s/\.matrix$//};
-
-
-#read in annotation, scitools approach
-if (!defined $ARGV[1]) {die $die2}
-else {read_annot($ARGV[1])};
-
-#define on your own which annot to include, scitools approach
-if (defined $opt{'a'}) {
-	@ANNOT_LIST = split(/,/, $opt{'a'});
-	foreach $annot (@ANNOT_LIST) {
-		$ANNOT_include{$annot} = 1;
+# get sparseMatrix components
+if ($ARGV[0] =~ /sparseMatrix/i) {  die "ERROR: SParsematrix support in progress!\n"; ######## SPARSE DIE ########
+	$sparse = 1;
+	if (defined $opt{'L'}) {
+		$col_file = $opt{'L'};
+	} else {
+		if (-e "$prefix.sparseMatrix.cols") {
+			$col_file = "$prefix.sparseMatrix.cols";
+		} elsif (-e "$prefix.sparseMatrix.cols.gz") {
+			$col_file = "$prefix.sparseMatrix.cols.gz";
+		} else {
+			die "ERROR: Cannot detect cols file (e.g. $prefix.sparseMatrix.cols), please provide as -L\n";
+		}
 	}
-}
-
-#define cells to keep
-if (defined $opt{'c'}) {
-	open IN, "$opt{'c'}";
+	if (defined $opt{'W'}) {
+		$row_file = $opt{'W'};
+	} else {
+		if (-e "$prefix.sparseMatrix.rows") {
+			$row_file = "$prefix.sparseMatrix.rows";
+		} elsif (-e "$prefix.sparseMatrix.rows.gz") {
+			$row_file = "$prefix.sparseMatrix.rows.gz";
+		} else {
+			die "ERROR: Cannot detect rows file (e.g. $prefix.sparseMatrix.rows), please provide as -W\n";
+		}
+	}
+	open IN, "$col_file"; @COLS = (); push @COLS, "cols";
 	while ($cellID = <IN>) {
 		chomp $cellID;
-		$CELLID_list_include{$cellID} = 1;
+		push @COLS, $cellID;
 	} close IN;
-}
-
-#define feature to keep
-if (defined $opt{'r'}) {
-	open IN, "$opt{'r'}";
+	open IN, "$row_file"; @ROWS = (); push @ROWS, "rows";
 	while ($rowID = <IN>) {
 		chomp $rowID;
-		$ROWID_list_include{$rowID} = 1;
+		push @ROWS, $rowID;
+	} close IN;
+} else {$sparse = 0};
+
+# load operations
+if (defined $opt{'o'}) {$operations = $opt{'O'}};
+@OPS = split(/,/, $operations);
+foreach $operation (@OPS) {
+	if ($operation =~ /(^a$|sum|add)/) {$OPS{'a'} = 1}
+	elsif ($operation =~ /(^m$|mean|average)/) {$OPS{'m'} = 1}
+	elsif ($operation =~ /(^d$|median)/) {$OPS{'d'} = 1}
+	elsif ($operation =~ /(^s$|stdev)/) {$OPS{'s'} = 1}
+	elsif ($operation =~ /(^c$|count)/) {$OPS{'c'} = 1}
+	else {die "ERROR: Cannot determine which operation '$operation' represents!\n"};
+}
+
+# get annot include
+if (defined $opt{'a'}) {
+	@ANNOT_INCLUDE = split(/,/, $opt{'a'};
+	foreach $annot (@ANNOT_INCLUDE) {
+		$ANNOT_include{$annot} = 1;
+	}
+} else {
+	@ANNOT_INCLUDE = ();
+}
+
+# read in annotations for cellIDs
+open IN, "$ARGV[1]";
+while ($l = <IN>) {
+	chomp $l;
+	($cellID,$annot_list) = split(/\t/, $l);
+	@{$CELLID_annots{$cellID}} = split(/,/, $annot_list);
+	foreach $annot (@{$CELLID_annots{$cellID}}) {
+		if (!defined $opt{'a'} || defined $ANNOT_include{$annot}) {
+			$ANNOT_CellIDs{$annot}{$cellID} = 1;
+			if (!defined $ANNOT_list{$annot} && !defined $opt{'a'}) {
+				push @ANNOT_INCLUDE, $annot;
+				$ANNOT_list{$annot} = 1;
+			}
+		}
+	}
+} close IN;
+
+# cellID sums for term-frequency
+if (defined $opt{'t'}) {
+	$rowCount = 0;
+	if ($sparse > 0.5) {
+		if ($ARGV[0] =~ /\.gz/) {
+			open IN, "$zcat $ARGV[0] |";
+		} else {
+			open IN, "$ARGV[0]";
+		}
+		while ($l = <IN>) {
+			chomp $l;
+			($rowNum,$cellNum,$value) = split(/\t/, $l);
+			$CELLID_sum{$COLS[$cellNum]} += $value;
+			if ($rowNum>$rowCount){$rowCount = $rowNum};
+		} close IN;
+	} else {
+		if ($ARGV[0] =~ /\.gz/) {
+			open IN, "$zcat $ARGV[0] |";
+		} else {
+			open IN, "$ARGV[0]";
+		}
+		$h = <IN>; chomp $h; @COLS = split(/\t/, $h);
+		while ($l = <IN>) {
+			$rowCount++;
+			@P = split(/\t/, $l);
+			$rowID = shift(@P);
+			for ($i = 0; $i < @COLS; $i++) {
+				$CELLID_sum{$COLS[$i]} += $P[$i];
+			}
+		} close IN;
+	}
+	if (defined $opt{'k'}) {
+		if ($opt{'k'} =~ /row/) {
+			$constant = $rowCount;
+		} else {
+			$constant = $opt{'k'};
+		}
+	} else {
+		$constant = $rowCount;
+	}
+}
+
+# perform operations row-by-row
+if ($sparse>0.5) {
+	# open OUTs
+	if (defined $OPS{'a'}) {if (defined $opt{'z'}) {open ADD, "| $gzip > $opt{'O'}.sparseMatrix.sum.gz"} else {open ADD, ">$opt{'O'}.sparseMatrix.sum"}};
+	if (defined $OPS{'m'}) {if (defined $opt{'z'}) {open MEAN, "| $gzip > $opt{'O'}.sparseMatrix.mean.gz"} else {open MEAN, ">$opt{'O'}.sparseMatrix.matrix"}};
+	if (defined $OPS{'d'}) {if (defined $opt{'z'}) {open MEDIAN, "| $gzip > $opt{'O'}.sparseMatrix.median.gz"} else {open MEDIAN, ">$opt{'O'}.sparseMatrix.median"}};
+	if (defined $OPS{'c'}) {if (defined $opt{'z'}) {open COUNT, "| $gzip > $opt{'O'}.sparseMatrix.count.gz"} else {open COUNT, ">$opt{'O'}.sparseMatrix.count"}};
+	if (defined $OPS{'s'}) {if (defined $opt{'z'}) {open STDEV, "| $gzip > $opt{'O'}.sparseMatrix.stdev.gz"} else {open STDEV, ">$opt{'O'}.sparseMatrix.stdev"}};
+	if (defined $opt{'z'}) {system("cp $row_file $opt{'O'}.sparseMatrix.rows")} else {system("cp $row_file $opt{'O'}.sparseMatrix.rows.gz")}
+	
+	###### PICK UP SPARSE HERE ######
+	
+} else {
+	# make column header
+	$col_names = join("\t", @ANNOT_INCLUDE);
+	# open OUTs
+	if (defined $OPS{'a'}) {
+		if (defined $opt{'z'}) {open ADD, "| $gzip > $opt{'O'}.sum.matrix.gz"} else {open ADD, ">$opt{'O'}.sum.matrix"};
+		print ADD "$col_names\n";
+	}
+	if (defined $OPS{'m'}) {if (defined $opt{'z'}) {open MEAN, "| $gzip > $opt{'O'}.mean.matrix.gz"} else {open MEAN, ">$opt{'O'}.mean.matrix"}};
+	if (defined $OPS{'d'}) {if (defined $opt{'z'}) {open MEDIAN, "| $gzip > $opt{'O'}.median.matrix.gz"} else {open MEDIAN, ">$opt{'O'}.median.matrix"}};
+	if (defined $OPS{'c'}) {if (defined $opt{'z'}) {open COUNT, "| $gzip > $opt{'O'}.count.matrix.gz"} else {open COUNT, ">$opt{'O'}.count.matrix"}};
+	if (defined $OPS{'s'}) {if (defined $opt{'z'}) {open STDEV, "| $gzip > $opt{'O'}.stdev.matrix.gz"} else {open STDEV, ">$opt{'O'}.stdev.matrix"}};
+	
+	if ($ARGV[0] =~ /\.gz/) {
+		open IN, "$zcat $ARGV[0] |";
+	} else {
+		open IN, "$ARGV[0]";
+	}
+	$h = <IN>; chomp $h; @COLS = split(/\t/, $h);
+	
+	# get cell counts for each annot
+	for ($i = 0; $i < @COLS; $i++) {
+		foreach $annot (keys %ANNOT_CellIDs) {
+			if (defined $ANNOT_CellIDs{$annot}{$COLS[$i]}) {
+				$ANNOT_cellCount{$annot}++;
+			}
+		}
+	}
+	
+	while ($l = <IN>) {
+		@P = split(/\t/, $l);
+		$rowID = shift(@P);
+		
+		# load in values
+		%ANNOT_values = ();
+		for ($i = 0; $i < @P; $i++) {
+			foreach $annot (keys %ANNOT_CellIDs) {
+				if (defined $ANNOT_CellIDs{$annot}{$COLS[$i]}) {
+					if (!defined $opt{'t'}) {
+						push @{$ANNOT_values{$annot}}, $P[$i];
+					} else {
+						$value = ($P[$i]/$CELLID_sum{$COLS[$i]})*$constant;
+						push @{$ANNOT_values{$annot}}, $value;
+					}
+				}
+			}
+		}
+		
+		if (defined $OPS{'c'}) {
+			print COUNT "$rowID";
+			for ($i = 0; $i < @ANNOT_INCLUDE; $i++) {
+				$annot = $ANNOT_INCLUDE[$i]; $nonZero = 0;
+				for ($j = 0; $j < @{$ANNOT_values{$annot}}; $j++) {
+					if ($ANNOT_values{$annot}[$j] != 0) {$nonZero++};
+				}
+				print COUNT "\t$nonZero";
+			}
+			print COUNT "\n";
+		}
+		if (defined $OPS{'d'}) {
+			print MEDIAN "$rowID";
+			for ($i = 0; $i < @ANNOT_INCLUDE; $i++) {
+				$annot = $ANNOT_INCLUDE[$i];
+				@SORTED = sort {$a<=>$b} @{$ANNOT_values{$annot}};
+				$median = $SORTED[int((@SORTED-1)/2)];
+				print MEDIAN "\t$median";
+			}
+			print MEDIAN "\n";
+		}
+		if (defined $OPS{'a'} || defined $OPS{'m'} || defined $OPS{'s'}) {
+			if (defined $OPS{'a'}) {print ADD "$rowID"; %ANNOT_sum = ()};
+			if (defined $OPS{'m'}) {print MEAN "$rowID"; %ANNOT_mean = ()};
+			if (defined $OPS{'s'}) {print STDEV "$rowID"; %ANNOT_stdev_sum = ()};
+			for ($i = 0; $i < @ANNOT_INCLUDE; $i++) {
+				$annot = $ANNOT_INCLUDE[$i];
+				for ($j = 0; $j < @{$ANNOT_values{$annot}}; $j++) {
+					$ANNOT_sum{$annot} += $ANNOT_values{$annot}[$j];
+				}
+				if (defined $OPS{'a'}) {print ADD "\t$ANNOT_sum{$annot}"};
+				if (defined $OPS{'m'} || defined $OPS{'s'}) {
+					$ANNOT_mean{$annot} = $ANNOT_sum{$annot}/$ANNOT_cellCount{$annot};
+				}
+				if (defined $OPS{'m'}) {print MEAN "\t$ANNOT_mean{$annot}"};
+				if (defined $OPS{'s'}) {
+					for ($j = 0; $j < @{$ANNOT_values{$annot}}; $j++) {
+						$ANNOT_stdev_sum{$annot} += ($ANNOT_values{$annot}[$j] - $ANNOT_mean{$annot})**2;
+					}
+					$stdev = sqrt($ANNOT_stdev_sum{$annot}/$ANNOT_cellCount{$annot});
+					print STDEV "\t$stdev";
+				}
+			}
+			if (defined $OPS{'a'}) {print ADD "\n"};
+			if (defined $OPS{'m'}) {print MEAN "\n"};
+			if (defined $OPS{'s'}) {print STDEV "\n"};
+		}
+		
 	} close IN;
 }
 
-#read in matrix scitools standard
-read_matrix($ARGV[0]);
-
-#define your matrix based on which cells and features are allowed to be kept
-#same style as CELL_FEATURE_VALUE
-%ANNOT_FEATURE_aggregate_value = ();
-#annot that we aggreagate by
-@MATRIX_aggregate_COLNAMES = ();
-
-if (defined $opt{'m'}) {
-	%ANNOT_FEATURE_list = ();
-	%ANNOT_FEATURE_aggregate_stdev = ();
-}
-
-#then the cells and aggregate signal
-foreach $cellID (keys %CELLID_FEATURE_value) {
-	#define cells to include based -c and -a options
-	#start out with all Cells being included
-	$includeCell = 1;
-	#remove cells that are not in include list provided
-	if (defined $opt{'c'} && !defined $CELLID_list_include{$cellID}) {$includeCell = 0};
-	
-	# handle multi-annot
-	@ANNOTS = split(/,/, $CELLID_annot{$cellID});
-	foreach $annot (@ANNOTS) {
-		#remove cells that are not in annot list provided
-		if (defined $opt{'a'} && !defined $ANNOT_include{$annot}) {$includeCell = 0};
-		if ($includeCell>0) {
-			foreach $rowID (keys %{$CELLID_FEATURE_value{$cellID}}) {
-				$includeRow = 1;
-				#define which rows to include based on -r option
-				if (defined $opt{'r'} && !defined $ROWID_list_include{$rowID}) {$includeRow = 0};
-				if ($includeRow>0 ) {
-					if (defined $opt{'m'}) {
-						push @{$ANNOT_FEATURE_list{$annot}{$rowID}}, $CELLID_FEATURE_value{$cellID}{$rowID};
-					} elsif (defined $opt{'b'}) {
-						if ($CELLID_FEATURE_value{$cellID}{$rowID}>0) {
-							#defined increase value by one
-							if (defined $ANNOT_FEATURE_aggregate_value{$annot}{$rowID}) {
-								$ANNOT_FEATURE_aggregate_value{$annot}{$rowID}++;
-							}
-							else {
-								#if not then value should become 1
-							$ANNOT_FEATURE_aggregate_value{$annot}{$rowID}=1;	
-							} 
-						} else {
-							#if not defined then give 0 value
-							if (!defined $ANNOT_FEATURE_aggregate_value{$annot}{$rowID}) {
-							$ANNOT_FEATURE_aggregate_value{$annot}{$rowID}=0;
-							}
-						}
-					} else {
-						if (defined $ANNOT_FEATURE_aggregate_value{$annot}{$rowID}) {
-							$ANNOT_FEATURE_aggregate_value{$annot}{$rowID}+=$CELLID_FEATURE_value{$cellID}{$rowID};
-						} else {
-							$ANNOT_FEATURE_aggregate_value{$annot}{$rowID}=$CELLID_FEATURE_value{$cellID}{$rowID};
-						}
-					}
-				}  
-			}
-			$included_cells++;
-			$CELLID_include{$cellID} = 1;
-			#define annotations to include
-			$ANNOT_include{$annot} = 1;  
-		}
-	}
-}
-
-if (defined $opt{'m'}) {
-	foreach $annot (keys %ANNOT_FEATURE_list) {
-		foreach $rowID (keys %{$ANNOT_FEATURE_list{$annot}}) {
-			$element_count = 0; $element_sum = 0;
-			foreach $value (@{$ANNOT_FEATURE_list{$annot}{$rowID}}) {
-				$element_count++; $element_sum += $value;
-			}
-			if ($element_count>0) {
-				$element_mean = $element_sum/$element_count;
-				$ANNOT_FEATURE_aggregate_value{$annot}{$rowID} = $element_mean;
-				$element_stdev_sum = 0;
-				foreach $value (@{$ANNOT_FEATURE_list{$annot}{$rowID}}) {
-					$element_stdev_sum += ($element_mean-$value)**2;
-				}
-				$ANNOT_FEATURE_aggregate_stdev{$annot}{$rowID} = sqrt($element_stdev_sum/$element_count);
-			} else {
-				$ANNOT_FEATURE_aggregate_value{$annot}{$rowID} = 0;
-				$ANNOT_FEATURE_aggregate_stdev{$annot}{$rowID} = 0;
-			}
-		}
-	}
-	%ANNOT_FEATURE_list = ();
-}
-
-#create a matrix of included annot
-foreach $annot (keys %ANNOT_include) {
-	push(@MATRIX_aggregate_COLNAMES,$annot);
-}
-
-#then look which rows are allowed to be kept
-foreach $rowID (keys %MATRIX_feature_nonZero) {
-	if (!defined $opt{'r'} || defined $ROWID_list_include{$rowID}) {
-		$ROWID_count{$rowID} = 0;
-		foreach $cellID (keys %CELLID_include) {
-			if ($CELLID_FEATURE_value{$cellID}{$rowID}>0) {
-				$ROWID_count{$rowID}++;
-			}
-		}
-		$ROWID_include{$rowID} = 1;
-		$included_rows++;
-	}       
-}
-
-#do write out depending on b option
-if (defined $opt{'m'}) {
-	open OUT, ">$opt{'O'}.mean.aggregate.matrix";
-	open SDV, ">$opt{'O'}.stdev.aggregate.matrix";
-	open LOG, ">$opt{'O'}.mean.aggregate.matrix.log"
-} elsif (defined $opt{'b'}) {
-	open OUT, ">$opt{'O'}.binary.aggregate.matrix";
-	open LOG, ">$opt{'O'}.binary.aggregate.matrix.log";
-} else {
-	open OUT, ">$opt{'O'}.aggregate.matrix";
-	open LOG, ">$opt{'O'}.aggregate.matrix.log";
-}
-
-#first define header
-$out_header = join("\t",@MATRIX_aggregate_COLNAMES);
-
-print OUT "$out_header\n";
-if (defined $opt{'m'}) {print SDV "$out_header\n"};
-
-for ($rowNum = 0; $rowNum < @MATRIX_ROWNAMES; $rowNum++) {
-	$rowID = $MATRIX_ROWNAMES[$rowNum];
-	if (defined $ROWID_include{$rowID}) {
-		print OUT "$rowID"; if (defined $opt{'m'}) {print SDV "$rowID"};
-		for ($aggrNum = 0; $aggrNum < @MATRIX_aggregate_COLNAMES; $aggrNum++) {
-			print OUT "\t$ANNOT_FEATURE_aggregate_value{$MATRIX_aggregate_COLNAMES[$aggrNum]}{$rowID}";
-			if (defined $opt{'m'}) {print SDV "\t$ANNOT_FEATURE_aggregate_stdev{$MATRIX_aggregate_COLNAMES[$aggrNum]}{$rowID}"};
-		} print OUT "\n"; if (defined $opt{'m'}) {print SDV "\n"};
-	}
-} close OUT;
-if (defined $opt{'m'}) {close SDV};
-
-$ts = localtime(time);
-print LOG "$ts scitools annot-aggr
-Matrix file = $ARGV[1]
-Annotation file = $ARGV[0]
-
-Options:
-";
-foreach $option (keys %opt) {
-	print LOG "   $option   $opt{$option}\n";
-}
-print LOG "Total cells in input: $matrix_colNum
-Total cells retained in output: $included_cells
-Total rows in input: $matrix_rowNum
-Total rows retained in output: $included_rows\n";
-close LOG;
-
 exit;
-}
 }
 1;
