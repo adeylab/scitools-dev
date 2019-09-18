01b0b3f6b13ddf92239e9575d749d570baaf43b7
acadey <adey@ohsu.edu.edu>
2019-09-18 10:09:52 -0700

    Create barnyard_compare.pm

---
+++ sci_commands/barnyard_compare.pm
@@ -0,0 +1,83 @@
+package sci_commands::barnyard_compare;
+
+use sci_utils::general;
+use Getopt::Std; %opt = ();
+use Exporter "import";
+@EXPORT = ("barnyard_compare");
+
+sub barnyard_compare {
+
+@ARGV = @_;
+getopts("O:q:n:f:s:", \%opt);
+
+$mapQ = 20;
+$minR = 1000;
+$maxF = 0.1;
+
+$die2 = "
+scitools barnyard-compare [options] [sorted rmdup filtered bam file]
+
+Generates a human vs mouse barnyard comparison file & stats.
+Reference must have _h or _m at the end of chromosome names to indicate species.
+
+Will exclude /(M|Y|L|K|G|Un|Random|Alt)/i chroms
+
+Options:
+   -O   [STR]   Output prefix (default is bam file prefix)
+   -q   [INT]   Mapping quality filter (def = $mapQ)
+   -n   [INT]   Min number of reads to consider cell (def = $minR)
+   -f   [FLT]   Max fraction of other species to be considered pure (def = $maxF)
+   -s   [STR]   Samtools call (def = $samtools)
+
+";
+
+if (!defined $ARGV[0]) {die $die2};
+if (!defined $opt{'O'}) {$opt{'O'} = $ARGV[0]; $opt{'O'} =~ s/\.bam$//};
+if (defined $opt{'s'}) {$samtools = $opt{'s'}};
+if (defined $opt{'n'}) {$minR = $opt{'n'}};
+if (defined $opt{'q'}) {$mapQ = $opt{'q'}};
+if (defined $opt{'f'}) {$maxF = $opt{'f'}};
+
+open IN, "$samtools view -q $mapQ $ARGV[0] |";
+while ($l = <IN>) {
+	chomp $l;
+	@P = split(/\t/, $l);
+	$barc = $P[0]; $barc =~ s/:.+$//;
+	if ($P[2] !~ /(M|Y|L|K|G|Un|Random|Alt)/i) {
+		if ($P[2] =~ /_h$/) {
+			$BARC_total{$barc}++;
+			$BARC_human{$barc}++;
+		} elsif ($P[2] =~ /_m$/) {
+			$BARC_total{$barc}++;
+			$BARC_mouse{$barc}++;
+		}
+	}
+} close IN;
+
+open OUT, ">$opt{'O'}.barnyard_cells.txt";
+foreach $barc (sort {$BARC_total{$b}<=>$BARC_total{$a}} keys %BARC_total) {
+	if ($BARC_total{$barc} >= $minR) {
+		$total_cells++;
+		$frac_h = sprintf("%.2f", $BARC_human{$barc}/$BARC_total{$barc});
+		print OUT "$barc\t$BARC_total{$barc}\t$BARC_human{$barc}\t$BARC_mouse{$barc}\t$frac_h\n";
+		$total_cells++;
+		if ($frac_h>=(1-$maxF) || $frac_h<=$maxF) {
+			$pure_cells++;
+		} else {
+			$mix_cells++;
+		}
+	}
+} close OUT;
+
+open OUT, ">$opt{'O'}.barnyard_stats.txt";
+$frac_mix = sprintf("%.2f", $mix_cells/$total_cells);
+$est_total_collision = $frac_mix*2;
+print OUT "Barnyard stats for $ARGV[0]
+Total cells with $minR reads: $total_cells
+Cells with > $maxF from other species: $mix_cells ($frac_mix)
+Estimated total collision (mix fraction * 2): $est_total_collision
+";
+close OUT;
+
+}
+1;
