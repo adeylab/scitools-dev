aa8e0084a96a0ad32402df72a8193d5fb6363e21
arsnishida <arsnishida@gmail.com>
2020-07-28 11:24:36 -0700

    Matrix DA Update
    - adds arguments for ranges of rows

--- sci_commands/matrix_DA.py
+++ sci_commands/matrix_DA.py
@@ -20,6 +20,10 @@ def main():
       help='metadata file, two column value file')
    p.add_argument('comparisons', action='store', type=str,
       help='comma separated list of strings to run pairwise comparisons, i.e. pg_1,pg_2,pg_3')
+   p.add_argument('range1', action='store', type=int,
+      help='0-based starting range of rows to look at')
+   p.add_argument('range2', action='store', type=int,
+      help='0-based starting range of rows to end at')
 
    p.add_argument('output', action='store', type=str,  
       help='output prefix')
@@ -68,10 +72,10 @@ def main():
       for num2,comp2 in enumerate(comps):
          if (num2 > num1):
             # create file
-            filename = args.output + "." + comp1 + "_" + comp2 + ".txt"
+            filename = args.output + "." + comp1 + "_" + comp2 + "." + str(args.range1) + "_" + str(args.range2) + ".txt"
             with open(filename, 'a') as OUTFILE:
                # for each row
-               for i in range(0,rownames.shape[0]):
+               for i in range(args.range1,args.range2):
                   name=rownames[i]
                   sub = C.tocsr()[i,:].todense()
                   a = sub[0,meta2.index.get_indexer_for((meta2[meta2[1] == comp1].index))]
