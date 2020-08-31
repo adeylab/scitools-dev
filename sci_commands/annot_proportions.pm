e6d700f713da6c78ee80eefa3d7b69922a30ae83
acadey <acadey@gmail.com>
2020-08-31 08:42:05 -0700

    Update annot_proportions.pm

--- sci_commands/annot_proportions.pm
+++ sci_commands/annot_proportions.pm
@@ -1,67 +1,67 @@
 package sci_commands::annot_proportions;
-
 use sci_utils::general;
 use Getopt::Std; %opt = ();
 use Exporter "import";
 @EXPORT = ("annot_proportions");
-
 sub annot_proportions {
-
 @ARGV = @_;
-getopts("O:A:", \%opt);
-
+getopts("O:", \%opt);
 $die2 = "
-scitools annot-proportions [options] -A [annot file] [check file]
-
-Produces proportions of annotations in respective file.
-Supports matrix, values, annot, list files (incl sparseMatrix cols)
-
+scitools annot-proportions [options] [annot1] [annot2]
+   or    proportions-annot
+Will produce a table of the number of cells present
+in each annotaiton combination. Only cells in both
+annot files will be included.
+Rows are annot1, columns are annot2.
 Options:
-   -O   [STR]   Output prefix (default is check file prefix)
-   -A   [STR]   Annotation file (required)
-   
+   -O   [STR]   Output prefix (def = annot1.annot2)
+                Adds .compare.txt suffix
 ";
-
-if (!defined $ARGV[0]) {die "Provide a check file!\n$die2"};
-if (!defined $opt{'A'}) {die "Provide an annotation file as -A option!\n$die2"};
-if (!defined $opt{'O'}) {$opt{'O'} = $ARGV[0]; $opt{'O'} =~ s/\.\D+$//};
-read_annot($opt{'A'});
-
-if ($ARGV[0] =~ /\.gz$/) {
-	open IN, "$zcat $ARGV[0] |";
-} else {
-	open IN, "$ARGV[0]";
+if (!defined $ARGV[1]) {die $die2};
+if (!defined $opt{'O'}) {
+	$pfx1 = $ARGV[0]; $pfx1 =~ s/\.annot$//;
+	$pfx2 = $ARGV[1]; $pfx2 =~ s/\.annot$//;
+	$opt{'O'} = "$pfx1.$pfx2";
 }
-
-if ($ARGV[0] =~ /\.matrix/) {
-	$l = <IN>; close IN; chomp $l;
-	@P = split(/\t/, $l);
-	foreach $cellID (@P) {
-		$CELLIDS{$cellID}++;
+$opt{'O'} =~ s/\.txt$//; $opt{'O'} =~ s/\.compare$//;
+$out_header = "#"; @ANNOT1 = ();
+open IN, "$ARGV[0]";
+while ($l = <IN>) {
+	chomp $l;
+	($cellID,$annot) = split(/\t/, $l);
+	$CELLID_annot1{$cellID} = $annot;
+	if (!defined $ANNOT1_check{$annot}) {
+		$out_header .= "\t$annot";
+		push @ANNOT1, $annot;
+		$ANNOT1_check{$annot} = 1;
 	}
-} else {
-	while ($l = <IN>) {
-		chomp $l;
-		@P = split(/\t/, $l);
-		$CELLIDS{$P[0]}++;
+} close IN;
+$cell_match_ct = 0; @ANNOT2 = ();
+open IN, "$ARGV[1]";
+while ($l = <IN>) {
+	chomp $l;
+	($cellID,$annot2) = split(/\t/, $l);
+	if (!defined $ANNOT2_check{$annot2}) {
+		push @ANNOT2, $annot2;
+		$ANNOT2_check{$annot2} = 1;
 	}
-	close IN;
+	if (defined $CELLID_annot1{$cellID}) {
+		$annot1 = $CELLID_annot1{$cellID};
+		$cell_match_ct++;
+		$ANNOT1_ANNOT2_ct{$annot1}{$annot2}++;
+	}
+} close IN;
+open OUT, ">$opt{'O'}.compare.txt";
+print OUT "$out_header\n";
+foreach $annot2 (@ANNOT2) {
+	print OUT "$annot2";
+	for ($i = 0; $i < @ANNOT1; $i++) {
+		if (!defined $ANNOT1_ANNOT2_ct{$ANNOT1[$i]}{$annot2}) {$ANNOT1_ANNOT2_ct{$ANNOT1[$i]}{$annot2}=0};
+		print OUT "\t$ANNOT1_ANNOT2_ct{$ANNOT1[$i]}{$annot2}";
+	}
+	print OUT "\n";
 }
-
-foreach $cellID (keys %CELLIDS) {
-	$cellCT++;
-	if (!defined $CELLID_annot{$cellID}) {
-		$CELLID_annot{$cellID} = "Undefined";
-	}
-	$annot = $CELLID_annot{$cellID};
-	$ANNOT_cellCT{$annot}++;
-}
-
-open OUT, ">$opt{'O'}.proportions.txt";
-foreach $annot (sort {$b<=>$a} keys %ANNOT_cellCT) {
-	$frac = sprintf("%.3f", $ANNOT_cellCT{$annot}/$cellCT);
-	print OUT "$annot\t$ANNOT_cellCT{$annot}\t$frac\n";
-} close OUT;
-
+close OUT;
+exit;
 }
 1;
